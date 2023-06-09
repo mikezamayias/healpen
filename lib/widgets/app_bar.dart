abefd126c385ab8269d6e89c9576a0623c67a13d
Mike Zamayias <29067825+mikezamayias@users.noreply.github.com>
2023-06-09 11:57:25 +0300

    improves appbar implementation

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -2,21 +2,30 @@ import 'package:flutter/material.dart' hide AppBar, Page;
 import 'package:flutter_screwdriver/flutter_screwdriver.dart';
 
 class AppBar extends StatelessWidget {
-  final String titleString;
+  final List<String> pathNames;
 
-  const AppBar({
-    Key? key,
-    required this.titleString,
-  }) : super(key: key);
+  const AppBar({Key? key, required this.pathNames}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      titleString,
-      style: context.theme.textTheme.headlineSmall!.copyWith(
-        color: context.theme.colorScheme.primary,
+    return RichText(
+      text: TextSpan(
+        children: [
+          for (int i = 0; i < pathNames.length; i++)
+            TextSpan(
+              text: i < pathNames.length - 1
+                  ? '${pathNames[i]}\u202F/\u202F'
+                  : pathNames[i],
+              style: (i < pathNames.length - 1)
+                  ? context.theme.textTheme.bodyLarge!.copyWith(
+                      color: context.theme.colorScheme.onBackground,
+                    )
+                  : context.theme.textTheme.headlineSmall!.copyWith(
+                      color: context.theme.colorScheme.primary,
+                    ),
+            ),
+        ],
       ),
-      maxLines: 1,
       overflow: TextOverflow.ellipsis,
     );
   }
