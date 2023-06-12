f2dcfd8b57f51e4c08378bbad8bd8f04de273d3b
Mike Zamayias <29067825+mikezamayias@users.noreply.github.com>
2023-06-12 10:52:16 +0300

    tweaks app bar

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -14,13 +14,13 @@ class AppBar extends StatelessWidget {
           for (int i = 0; i < pathNames.length; i++)
             TextSpan(
               text: i < pathNames.length - 1
-                  ? '${pathNames[i]}\u202F/\u202F'
+                  ? '${pathNames[i]} / '
                   : pathNames.length != 1 && pathNames.length > 2
                       ? '\n${pathNames[i]}'
                       : pathNames[i],
               style: (i < pathNames.length - 1)
                   ? context.theme.textTheme.headlineSmall!.copyWith(
-                      color: context.theme.colorScheme.onBackground,
+                      color: context.theme.colorScheme.outline,
                     )
                   : context.theme.textTheme.headlineMedium!.copyWith(
                       color: context.theme.colorScheme.primary,
