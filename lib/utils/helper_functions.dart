251d9d59ab48adc99112b826321d17219c51cb0f
Mike Zamayias <29067825+mikezamayias@users.noreply.github.com>
2023-06-13 11:15:08 +0300

    removes dead code

--- lib/utils/helper_functions.dart
+++ lib/utils/helper_functions.dart
@@ -1,7 +1,4 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import '../enums/app_theming.dart';
 import '../themes/blueprint_theme.dart';
@@ -38,49 +35,3 @@ ThemeData getTheme(AppColor appColor, Brightness brightness) =>
           brightness,
         ),
     };
-
-Future writeAppearance(Appearance appearance) async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  prefs.setString('appearance', '$appearance');
-  log(
-    '$appearance',
-    name: 'helper_functions.dart:appearance',
-  );
-}
-
-Future readAppearance(Appearance appearance) async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  String? value = prefs.getString('appearance');
-  if (value != null) {
-    appearance = Appearance.values.firstWhere(
-      (e) => e.toString() == value,
-    );
-  }
-  log(
-    value ?? 'null',
-    name: 'helper_functions.dart:readAppearance',
-  );
-}
-
-Future writeColor(AppColor appColor) async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  prefs.setString('color', '$appColor');
-  log(
-    '$appColor',
-    name: 'helper_functions.dart:writeColor',
-  );
-}
-
-Future readColor(AppColor appColor) async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  String? value = prefs.getString('color');
-  if (value != null) {
-    appColor = AppColor.values.firstWhere(
-      (e) => e.toString() == value,
-    );
-  }
-  log(
-    value ?? 'null',
-    name: 'helper_functions.dart:readColor',
-  );
-}
