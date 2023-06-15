a6c8cd15a43474c564a9afc19d9a71a4c722d954
Mike Zamayias <29067825+mikezamayias@users.noreply.github.com>
2023-06-15 14:07:02 +0300

    adds dialog that explains a priavte note

---
+++ lib/widgets/custom_dialog.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screwdriver/flutter_screwdriver.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+import '../utils/constants.dart';
+import 'custom_list_tile/custom_list_tile.dart';
+
+class CustomDialog extends StatelessWidget {
+  final String titleString;
+  final String contentString;
+  final List<CustomListTile> actions;
+
+  const CustomDialog({
+    super.key,
+    required this.titleString,
+    required this.contentString,
+    required this.actions,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      iconPadding: EdgeInsets.zero,
+      insetPadding: EdgeInsets.zero,
+      buttonPadding: EdgeInsets.zero,
+      contentPadding: EdgeInsets.zero,
+      titlePadding: EdgeInsets.all(gap),
+      actionsPadding: EdgeInsets.only(
+        left: gap,
+        right: gap,
+        bottom: gap,
+      ),
+      backgroundColor: context.theme.colorScheme.primaryContainer,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(radius),
+      ),
+      title: Container(
+        width: 75.w,
+        decoration: BoxDecoration(
+          color: context.theme.colorScheme.background,
+          borderRadius: BorderRadius.circular(radius - gap),
+        ),
+        padding: EdgeInsets.all(gap),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              titleString,
+              style: context.theme.textTheme.headlineSmall!.copyWith(
+                color: context.theme.colorScheme.onBackground,
+              ),
+            ),
+            SizedBox(
+              height: gap,
+            ),
+            Text(
+              contentString,
+              style: context.theme.textTheme.bodyLarge!.copyWith(
+                color: context.theme.colorScheme.onBackground,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+      actionsAlignment: MainAxisAlignment.center,
+      actions: actions,
+    );
+  }
+}
