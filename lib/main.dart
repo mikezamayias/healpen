9ada36a4931a6cbccba0661785498c31e18c5b27
Mike Zamayias <29067825+mikezamayias@users.noreply.github.com>
2023-06-19 13:57:21 +0300

    refactors main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -102,59 +104,77 @@ void main() async {
                 theme: theme,
                 home: AuthFlowBuilder<EmailLinkAuthController>(
                   provider: emailLinkAuthProvider,
-                  builder: (context, state, ctrl, child) {
+                  builder: (context, AuthState state, ctrl, child) {
                     if (state is SignedIn) {
                       return const Healpen();
-                    } else {
-                      return BlueprintView(
-                        appBar: const AppBar(
-                          pathNames: ['Passwordless Sign In'],
+                    }
+
+                    // We know the state is not SignedIn, so we can handle other cases separately
+                    Widget body;
+                    if (state is Uninitialized) {
+                      body = CustomListTile(
+                        cornerRadius: radius,
+                        contentPadding: const EdgeInsets.all(12),
+                        title: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Sign in using only your email.',
+                              style: context.theme.textTheme.titleLarge,
+                            ),
+                          ],
                         ),
-                        body: switch (state) {
-                          Uninitialized() => CustomListTile(
-                              cornerRadius: radius,
-                              contentPadding: const EdgeInsets.all(12),
-                              title: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    'Sign in using only your email.',
-                                    style: context.theme.textTheme.titleLarge,
-                                  ),
-                                ],
-                              ),
-                              subtitle: TextField(
-                                decoration: InputDecoration(
-                                  hintText: 'Email',
-                                  hintStyle: context.theme.textTheme.titleLarge,
-                                ),
-                                style: context.theme.textTheme.titleLarge,
-                                onSubmitted: (String email) {
-                                  ctrl.sendLink(email);
-                                },
-                              ),
-                            ),
-                          AwaitingDynamicLink() => const LoadingTile(
-                              durationTitle: 'Sending link to your email',
-                            ),
-                          AuthFailed() => CustomListTile(
-                              cornerRadius: radius,
-                              contentPadding: const EdgeInsets.all(12),
-                              backgroundColor: context.theme.colorScheme.error,
-                              textColor: context.theme.colorScheme.onError,
-                              titleString: 'Something went wrong!',
-                              subtitle: ErrorText(exception: state.exception),
-                            ),
-                          _ => CustomListTile(
-                              cornerRadius: radius,
-                              contentPadding: const EdgeInsets.all(12),
-                              titleString: 'Unknown state',
-                              subtitleString: '${state.runtimeType}',
-                            )
-                        },
+                        subtitle: TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Email',
+                            hintStyle: context.theme.textTheme.titleLarge,
+                          ),
+                          style: context.theme.textTheme.titleLarge,
+                          onSubmitted: (String email) {
+                            ctrl.sendLink(email);
+                          },
+                        ),
+                      );
+                    } else if (state is AwaitingDynamicLink) {
+                      body = const LoadingTile(
+                        durationTitle: 'Sending link to your email',
+                      );
+                    } else if (state is SigningIn) {
+                      body = CustomListTile(
+                        cornerRadius: radius,
+                        contentPadding: const EdgeInsets.all(12),
+                        titleString: 'Please check your email inbox',
+                      );
+                    } else if (state is AuthFailed) {
+                      log(state.exception.toString(), name: 'AuthFailed');
+                      body = CustomListTile(
+                        cornerRadius: radius,
+                        contentPadding: const EdgeInsets.all(12),
+                        backgroundColor: context.theme.colorScheme.error,
+                        textColor: context.theme.colorScheme.onError,
+                        titleString: 'Something went wrong!',
+                        subtitle: ErrorText(exception: state.exception),
+                      );
+                    } else {
+                      log(
+                        '${state.runtimeType.toString()}:${state.toString()}',
+                        name: 'state:else',
+                      );
+                      body = CustomListTile(
+                        cornerRadius: radius,
+                        contentPadding: const EdgeInsets.all(12),
+                        titleString: 'Unknown state',
+                        subtitleString: '${state.runtimeType}',
                       );
                     }
+                    // Only need one return statement
+                    return BlueprintView(
+                      appBar: const AppBar(
+                        pathNames: ['Passwordless Sign In'],
+                      ),
+                      body: body,
+                    );
                   },
                 ),
               );
