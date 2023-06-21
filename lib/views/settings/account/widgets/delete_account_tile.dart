cbeba301227fec224c9228060f17a5138fa0e0fc
Mike Zamayias <29067825+mikezamayias@users.noreply.github.com>
2023-06-21 15:36:50 +0300

    works on account deletion flow

--- lib/views/settings/account/widgets/delete_account_tile.dart
+++ lib/views/settings/account/widgets/delete_account_tile.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter_screwdriver/flutter_screwdriver.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../../../utils/constants.dart';
+import '../../../../widgets/custom_dialog.dart';
 import '../../../../widgets/custom_list_tile/custom_list_tile.dart';
 
 class DeleteAccountTile extends StatelessWidget {
@@ -19,9 +20,80 @@ class DeleteAccountTile extends StatelessWidget {
       ),
       titleString: 'Delete Account',
       leadingIconData: FontAwesomeIcons.trash,
-      onTap: () async {
-        await HapticFeedback.heavyImpact();
-        // context.read(authProvider).signOut();
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return CustomDialog(
+              backgroundColor: context.theme.colorScheme.tertiaryContainer,
+              titleString: 'You are about to delete your account.',
+              contentString:
+                  'Are you sure you want to delete your account?\n\nYour data will be deleted and you will be signed out of the app.',
+              actions: [
+                CustomListTile(
+                  cornerRadius: radius - gap,
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: gap * 2,
+                    vertical: gap,
+                  ),
+                  onTap: () => context.navigator.pop(),
+                  titleString: 'Cancel',
+                  responsiveWidth: true,
+                ),
+                CustomListTile(
+                  cornerRadius: radius - gap,
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: gap * 2,
+                    vertical: gap,
+                  ),
+                  onTap: () => context.navigator.pop(),
+                  titleString: 'Proceed',
+                  backgroundColor: context.theme.colorScheme.tertiary,
+                  textColor: context.theme.colorScheme.onTertiary,
+                  responsiveWidth: true,
+                ),
+              ],
+            );
+          },
+        );
+        // User user = FirebaseAuth.instance.currentUser!;
+        // log('$user', name: 'Deleting user');
+        // user.delete().onError(
+        //   (error, stackTrace) {
+        //     log('$error', name: 'Error deleting user');
+        //     showDialog(
+        //       context: context,
+        //       builder: (BuildContext context) {
+        //         return CustomDialog(
+        //           titleString: 'Error deleting account',
+        //           contentString: 'Please sign in again.',
+        //           actions: [
+        //             CustomListTile(
+        //               onTap: () => context.navigator.push(
+        //                 MaterialPageRoute(
+        //                   builder: (context) => const AuthView(),
+        //                 ),
+        //               ),
+        //               titleString: 'OK',
+        //             ),
+        //           ],
+        //         );
+        //       },
+        //     );
+        //   },
+        // ).then(
+        //   (_) {
+        //     context.navigator
+        //         .pushReplacement(
+        //           MaterialPageRoute(
+        //             builder: (context) => const AuthView(),
+        //           ),
+        //         )
+        //         .whenComplete(
+        //           () => HapticFeedback.heavyImpact(),
+        //         );
+        //   },
+        // );
       },
     );
   }
