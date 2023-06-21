e8ed715b155ce668b4dd01e41d5e890e767d3a9a
Mike Zamayias <29067825+mikezamayias@users.noreply.github.com>
2023-06-21 14:55:34 +0300

    messing around with logic and auth views

--- lib/views/login/login_view.dart
+++ lib/views/login/login_view.dart
@@ -1,48 +1,83 @@
-import 'dart:developer';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart' hide AppBar;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screwdriver/flutter_screwdriver.dart';
 
-import '../../utils/constants.dart';
 import '../../widgets/app_bar.dart';
 import '../../widgets/custom_list_tile/custom_list_tile.dart';
-import '../../widgets/loading_tile.dart';
 import '../blueprint/blueprint_view.dart';
-import '../writing/writing_view.dart';
 
-class LoginView extends StatefulWidget {
+class LoginView extends ConsumerStatefulWidget {
   const LoginView({super.key});
 
   @override
-  State<LoginView> createState() => _LoginViewState();
+  ConsumerState<LoginView> createState() => LoginViewState();
 }
 
-class _LoginViewState extends State<LoginView>
+class LoginViewState extends ConsumerState<LoginView>
     implements EmailLinkAuthListener {
-  ActionCodeSettings actionCodeSettings = ActionCodeSettings(
-    url: 'https://healpen.page.link',
-    handleCodeInApp: true,
-    androidMinimumVersion: '1',
-    androidPackageName: 'com.mikezamayias.healpen',
-    iOSBundleId: 'com.mikezamayias.healpen',
-  );
-
   @override
   final auth = FirebaseAuth.instance;
 
   @override
-  late final EmailLinkAuthProvider provider =
-      EmailLinkAuthProvider(actionCodeSettings: actionCodeSettings)
-        ..authListener = this
-        ..auth = auth;
+  late EmailLinkAuthProvider provider;
+
+  @override
+  void initState() {
+    super.initState();
+    provider = EmailLinkAuthProvider(
+      actionCodeSettings: ActionCodeSettings(
+        url: 'https://healpen.page.link',
+        handleCodeInApp: true,
+        androidMinimumVersion: '1',
+        androidPackageName: 'com.mikezamayias.healpen',
+        iOSBundleId: 'com.mikezamayias.healpen',
+      ),
+    )..authListener = this;
+  }
+
+  // @override
+  // late final EmailLinkAuthProvider provider = EmailLinkAuthProvider(
+  //   actionCodeSettings: ActionCodeSettings(
+  //     url: 'https://healpen.page.link',
+  //     handleCodeInApp: true,
+  //     androidMinimumVersion: '1',
+  //     androidPackageName: 'com.mikezamayias.healpen',
+  //     iOSBundleId: 'com.mikezamayias.healpen',
+  //   ),
+  // )..authListener = this;
+
+  late Widget body = CustomListTile(
+    selectableText: true,
+    titleString: 'Sign in using only your email',
+    subtitle: TextField(
+      decoration: InputDecoration(
+        hintText: 'Email',
+        hintStyle: context.theme.textTheme.titleLarge,
+      ),
+      style: context.theme.textTheme.titleLarge,
+      onSubmitted: provider.sendLink,
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return BlueprintView(
+      appBar: const AppBar(
+        pathNames: ['Password-less Sign In'],
+      ),
+      body: body,
+    );
+  }
 
   @override
   void onBeforeLinkSent(String email) {
     setState(() {
-      child = const LoadingTile(
-        durationTitle: 'Sending link to your email',
+      body = CustomListTile(
+        selectableText: true,
+        titleString: 'onBeforeLinkSent',
+        subtitleString: 'Sending link to $email',
       );
     });
   }
@@ -50,9 +85,10 @@ class _LoginViewState extends State<LoginView>
   @override
   void onLinkSent(String email) {
     setState(() {
-      child = CustomListTile(
-        titleString: 'Please check your email.',
-        cornerRadius: radius,
+      body = CustomListTile(
+        selectableText: true,
+        titleString: 'onLinkSent',
+        subtitleString: 'Link sent to $email',
       );
     });
   }
@@ -60,150 +96,95 @@ class _LoginViewState extends State<LoginView>
   @override
   void onBeforeProvidersForEmailFetch() {
     setState(() {
-      child = const CircularProgressIndicator();
+      body = const CustomListTile(
+        selectableText: true,
+        titleString: 'onBeforeProvidersForEmailFetch',
+        subtitleString: 'Fetching providers for email',
+      );
     });
   }
 
   @override
   void onBeforeSignIn() {
     setState(() {
-      child = const CircularProgressIndicator();
+      body = const CustomListTile(
+        selectableText: true,
+        titleString: 'onBeforeSignIn',
+        subtitleString: 'Authentication process starts',
+      );
     });
   }
 
   @override
   void onCanceled() {
     setState(() {
-      child = const Text('Authenticated cancelled');
+      body = const CustomListTile(
+        selectableText: true,
+        titleString: 'onCanceled',
+        subtitleString: 'User canceled the sign in process',
+      );
     });
   }
 
   @override
   void onCredentialLinked(AuthCredential credential) {
-    log(
-      credential.toString(),
-      name: 'onCredentialLinked',
-    );
-    Navigator.of(context).pushReplacement(
-      MaterialPageRoute(builder: (context) => const WritingView()),
-    );
+    setState(() {
+      body = const CustomListTile(
+        selectableText: true,
+        titleString: 'onCredentialLinked',
+        subtitleString: 'Credential linked with current user',
+      );
+    });
   }
 
   @override
   void onDifferentProvidersFound(
-    String email,
-    List<String> providers,
-    AuthCredential? credential,
-  ) {
-    showDifferentMethodSignInDialog(
-      context: context,
-      availableProviders: providers,
-      providers: FirebaseUIAuth.providersFor(FirebaseAuth.instance.app),
-    );
+      String email, List<String> providers, AuthCredential? credential) {
+    // Handle case of different providers found
   }
 
   @override
   void onError(Object error) {
-    if (error is FirebaseException && error.code == 'too-many-requests') {
-      // handle the too-many-requests error
-      setState(() {
-        child = CustomListTile(
-          backgroundColor: context.theme.colorScheme.tertiary,
-          textColor: context.theme.colorScheme.onTertiary,
-          cornerRadius: radius,
-          titleString: 'Error',
-          subtitle: Text(
-            'Too many requests. Try again later.',
-            style: context.theme.textTheme.titleSmall,
-          ),
-        );
-      });
-    } else {
-      try {
-        // tries default recovery strategy
-        defaultOnAuthError(provider, error);
-      } catch (err) {
-        setState(() {
-          defaultOnAuthError(provider, error);
-        });
-      }
-    }
-  }
-
-  @override
-  void onSignedIn(UserCredential credential) {
-    log(
-      credential.toString(),
-      name: 'onSignedIn',
-    );
-    Navigator.of(context).pushReplacement(
-      MaterialPageRoute(builder: (context) => const WritingView()),
-    );
+    setState(() {
+      body = CustomListTile(
+        selectableText: true,
+        titleString: 'onError',
+        subtitleString: error.toString(),
+      );
+    });
   }
 
   @override
   void onCredentialReceived(AuthCredential credential) {
-    log(
-      credential.toString(),
-      name: 'onCredentialReceived',
-    );
     setState(() {
-      child = const Text('onCredentialReceived');
+      body = const CustomListTile(
+        selectableText: true,
+        titleString: 'onCredentialReceived',
+        subtitleString:
+            'Linking the credential with currently signed in user account',
+      );
     });
   }
 
   @override
   void onMFARequired(MultiFactorResolver resolver) {
     setState(() {
-      child = const Text('Expecting MFA widget');
+      body = const CustomListTile(
+        selectableText: true,
+        titleString: 'onMFARequired',
+        subtitleString: 'User has to complete MFA',
+      );
     });
   }
 
-  late Widget child = Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      CustomListTile(
-        cornerRadius: radius,
-        contentPadding: const EdgeInsets.all(12),
-        title: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Enter your email',
-              style: context.theme.textTheme.titleLarge,
-            ),
-          ],
-        ),
-        subtitle: TextField(
-          controller: emailController,
-          keyboardType: TextInputType.emailAddress,
-          decoration: InputDecoration(
-            hintText: 'Email',
-            hintStyle: context.theme.textTheme.titleLarge,
-          ),
-          style: context.theme.textTheme.titleLarge,
-        ),
-      ),
-      SizedBox(height: gap),
-      CustomListTile(
-        cornerRadius: radius,
-        responsiveWidth: true,
-        titleString: 'Send link to email',
-        onTap: () => provider.sendLink(emailController.text),
-      )
-    ],
-  );
-
-  final emailController = TextEditingController();
-
   @override
-  Widget build(BuildContext context) {
-    return BlueprintView(
-      appBar: const AppBar(
-        pathNames: ['Passwordless Sign In'],
-      ),
-      body: child,
-    );
+  void onSignedIn(UserCredential credential) {
+    setState(() {
+      body = CustomListTile(
+        selectableText: true,
+        titleString: 'Signed in successfully',
+        subtitleString: 'User: ${credential.user?.email}',
+      );
+    });
   }
 }
