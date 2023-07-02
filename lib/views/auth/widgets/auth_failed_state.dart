b0249cc3be9bbfc439f7b002a280a38349e80fb7
Mike Zamayias <29067825+mikezamayias@users.noreply.github.com>
2023-07-03 01:49:42 +0300

    refactors failed auth state widget

--- lib/views/auth/widgets/auth_failed_state.dart
+++ lib/views/auth/widgets/auth_failed_state.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart' hide AppBar, Divider;
 import 'package:flutter_screwdriver/flutter_screwdriver.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../../utils/constants.dart';
 import '../../../widgets/custom_list_tile/custom_list_tile.dart';
 
 class AuthFailedState extends StatelessWidget {
   final AuthState state;
+
   const AuthFailedState({
     Key? key,
     required this.state,
@@ -22,12 +24,32 @@ class AuthFailedState extends StatelessWidget {
         children: [
           CustomListTile(
             cornerRadius: radius,
-            contentPadding: const EdgeInsets.all(12),
+            contentPadding: EdgeInsets.symmetric(
+              horizontal: gap * 2,
+              vertical: gap,
+            ),
+            leadingIconData: FontAwesomeIcons.circleExclamation,
             selectableText: true,
             backgroundColor: context.theme.colorScheme.error,
             textColor: context.theme.colorScheme.onError,
-            titleString: 'Something went wrong!',
-            subtitleString: (state as AuthFailed).exception.toString(),
+            titleString: 'Something went wrong',
+            subtitleString:
+                (state as AuthFailed).exception.toString().split('] ').last,
+          ),
+          SizedBox(height: gap),
+          CustomListTile(
+            cornerRadius: radius,
+            responsiveWidth: true,
+            contentPadding: EdgeInsets.symmetric(
+              horizontal: gap * 2,
+              vertical: gap,
+            ),
+            leadingIconData: FontAwesomeIcons.arrowLeft,
+            selectableText: true,
+            // backgroundColor: context.theme.colorScheme.error,
+            // textColor: context.theme.colorScheme.onError,
+            titleString: 'Go back',
+            onTap: () => context.navigator.pushReplacementNamed('/auth'),
           ),
         ],
       ),
