0c37a0a72e7e878bc6551d0b7bbeee45edfcf803
Mike Zamayias <29067825+mikezamayias@users.noreply.github.com>
2023-07-03 01:50:03 +0300

    editing email is working

--- lib/views/settings/account/widgets/edit_email_tile.dart
+++ lib/views/settings/account/widgets/edit_email_tile.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screwdriver/flutter_screwdriver.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:validated/validated.dart' as validated;
@@ -10,15 +11,15 @@ import 'package:validated/validated.dart' as validated;
 import '../../../../utils/constants.dart';
 import '../../../../widgets/custom_list_tile/custom_list_tile.dart';
 
-class EditEmailTile extends StatefulWidget {
+class EditEmailTile extends ConsumerStatefulWidget {
   const EditEmailTile({Key? key}) : super(key: key);
 
   @override
-  State<EditEmailTile> createState() => _EditEmailTileState();
+  ConsumerState<EditEmailTile> createState() => _EditEmailTileState();
 }
 
-class _EditEmailTileState extends State<EditEmailTile> {
-  final currentUserEmailAddress = FirebaseAuth.instance.currentUser?.email;
+class _EditEmailTileState extends ConsumerState<EditEmailTile> {
+  final currentUser = FirebaseAuth.instance.currentUser!;
   final _emailController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
 
@@ -33,8 +34,7 @@ class _EditEmailTileState extends State<EditEmailTile> {
             TextFormField(
               controller: _emailController,
               decoration: InputDecoration(
-                hintText:
-                    currentUserEmailAddress ?? 'What is your email address?',
+                hintText: currentUser.email ?? 'What is your email address?',
                 hintStyle: context.theme.textTheme.titleLarge,
                 helperStyle: context.theme.textTheme.bodyMedium!.copyWith(
                   color: context.theme.colorScheme.secondary,
@@ -49,12 +49,12 @@ class _EditEmailTileState extends State<EditEmailTile> {
             SizedBox(height: gap),
             CustomListTile(
               responsiveWidth: true,
-              titleString: 'Save Email',
               cornerRadius: radius,
               contentPadding: EdgeInsets.symmetric(
-                vertical: gap,
                 horizontal: gap * 2,
+                vertical: gap,
               ),
+              titleString: 'Save Email',
               leadingIconData: FontAwesomeIcons.solidFloppyDisk,
               onTap: () {
                 if (_formKey.currentState!.validate()) {
@@ -62,27 +62,70 @@ class _EditEmailTileState extends State<EditEmailTile> {
                     _emailController.text,
                     name: '_LoginViewState:build:email',
                   );
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      duration: 6.seconds,
-                      backgroundColor: Colors.transparent,
-                      content: CustomListTile(
-                        backgroundColor: context.theme.colorScheme.secondary,
-                        title: Text(
-                          'Your email has been updated.',
-                          style: context.theme.textTheme.bodyLarge!.copyWith(
-                            color: context.theme.colorScheme.onSecondary,
-                          ),
+                  currentUser
+                      .reauthenticateWithCredential(
+                        EmailAuthProvider.credential(
+                          email: currentUser.email!,
+                          password: currentUser.email!,
                         ),
-                        contentPadding: EdgeInsets.symmetric(
-                          vertical: gap,
-                          horizontal: gap * 2,
+                      )
+                      .then(
+                        (value) =>
+                            currentUser.updateEmail(_emailController.text).then(
+                          (_) {
+                            log(
+                              '$currentUser',
+                              name: '_LoginViewState:build:updateEmail:then',
+                            );
+                            return ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                duration: 6.seconds,
+                                content: CustomListTile(
+                                  backgroundColor:
+                                      context.theme.colorScheme.secondary,
+                                  title: Text(
+                                    'Your email has been updated.',
+                                    style: context.theme.textTheme.bodyLarge!
+                                        .copyWith(
+                                      color:
+                                          context.theme.colorScheme.onSecondary,
+                                    ),
+                                  ),
+                                  contentPadding: EdgeInsets.symmetric(
+                                    vertical: gap,
+                                    horizontal: gap * 2,
+                                  ),
+                                  cornerRadius: radius,
+                                ),
+                              ),
+                            );
+                          },
+                        ).onError(
+                          (error, stackTrace) {
+                            log(
+                              '$currentUser',
+                              name: '_LoginViewState:build:updateEmail:onError',
+                            );
+                            return ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                duration: 6.seconds,
+                                content: CustomListTile(
+                                  backgroundColor:
+                                      context.theme.colorScheme.error,
+                                  textColor: context.theme.colorScheme.onError,
+                                  titleString: 'Something went wrong!',
+                                  subtitleString: error.toString(),
+                                  contentPadding: EdgeInsets.symmetric(
+                                    vertical: gap,
+                                    horizontal: gap * 2,
+                                  ),
+                                  cornerRadius: radius,
+                                ),
+                              ),
+                            );
+                          },
                         ),
-                        cornerRadius: radius,
-                      ),
-                      padding: EdgeInsets.all(gap * 2),
-                    ),
-                  );
+                      );
                 }
               },
             ),
