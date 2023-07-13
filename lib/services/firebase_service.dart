a685a4569fb50fdf5700e90c4628ee6c646753db
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-07-13 13:18:41 +0300

    completely removes firebase app check

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
@@ -26,14 +25,6 @@ class FirebaseService {
 
     /// Initializes Firebase Analytics
     FirebaseAnalytics.instance;
-
-    /// Initializes Firebase AppCheck
-    await FirebaseAppCheck.instance.activate(
-      // androidProvider: AndroidProvider.playIntegrity,
-      // androidProvider: AndroidProvider.debug,
-      appleProvider: AppleProvider.appAttest,
-      // appleProvider: AppleProvider.debug,
-    );
 
     /// Initializes Firebase Crashlytics
     // Pass all uncaught errors from the framework to Crashlytics.
