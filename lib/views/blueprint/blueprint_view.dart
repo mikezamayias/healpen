85f5e67a771782ce5dee2dd0ba092cbdd47e32fd
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-07-14 13:02:55 +0300

    adds padding back

--- lib/views/blueprint/blueprint_view.dart
+++ lib/views/blueprint/blueprint_view.dart
@@ -22,7 +22,7 @@ class BlueprintView extends ConsumerWidget {
       onTap: () => context.focusScope.unfocus(),
       child: Container(
         color: context.theme.scaffoldBackgroundColor,
-        padding: EdgeInsets.only(left: gap, right: gap, bottom: gap),
+        padding: EdgeInsets.all(gap),
         child: SafeArea(
           child: Scaffold(
             backgroundColor: Colors.transparent,
