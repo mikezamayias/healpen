6272b03c456ff403b11ab7d5c66c5346b41cfb3d
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-07-16 07:24:52 +0300

    adds function to change system bar styles

--- lib/utils/helper_functions.dart
+++ lib/utils/helper_functions.dart
@@ -1,12 +1,42 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screwdriver/flutter_screwdriver.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../enums/app_theming.dart';
 import '../providers/settings_providers.dart';
 import '../themes/blueprint_theme.dart';
+
+void updateSystemBarStyles(BuildContext context, WidgetRef ref) {
+  log(
+    '${context.mediaQuery.platformBrightness}',
+    name: 'updateSystemBarStyles',
+  );
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    systemNavigationBarColor: context.theme.colorScheme.background,
+    statusBarColor: context.theme.colorScheme.background,
+    statusBarBrightness: switch (ref.watch(appearanceProvider)) {
+      Appearance.system =>
+        context.mediaQuery.platformBrightness == Brightness.light
+            ? Brightness.dark
+            : Brightness.light,
+      Appearance.light => Brightness.light,
+      Appearance.dark => Brightness.dark,
+    },
+    statusBarIconBrightness: context.theme.colorScheme.background.isLight
+        ? Brightness.dark
+        : Brightness.light,
+    systemNavigationBarIconBrightness:
+        context.theme.colorScheme.background.isLight
+            ? Brightness.dark
+            : Brightness.light,
+    systemStatusBarContrastEnforced: true,
+    systemNavigationBarContrastEnforced: true,
+  ));
+}
 
 ThemeData createTheme(Color color, Brightness brightness) {
   return blueprintTheme(
