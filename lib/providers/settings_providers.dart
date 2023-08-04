4b7676c1280ff11e246e681d5a0d72ff8adaaf71
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-08-05 00:27:23 +0300

    adds provider for writing setting

--- lib/providers/settings_providers.dart
+++ lib/providers/settings_providers.dart
@@ -9,3 +9,10 @@ final appearanceProvider = StateProvider<Appearance>(
 final appColorProvider = StateProvider<AppColor>(
   (ref) => AppColor.pastelOcean,
 );
+
+/// Whether to reset the stopwatch when the user erases all the text.
+/// When set to true, the stopwatch will continue running until the user saves.
+/// When set to false, the stopwatch will reset when the user erases all the text.
+final writingResetStopwatchOnEmptyProvider = StateProvider<bool>(
+  (ref) => true,
+);
