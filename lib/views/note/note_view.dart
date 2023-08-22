42a17818fde4ff545ea1943e410425f858fcb88f
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-08-22 15:28:28 +0300

    adds sentiment analysis in ui

--- lib/views/note/note_view.dart
+++ lib/views/note/note_view.dart
@@ -1,7 +1,6 @@
-import 'dart:developer';
-
 import 'package:dart_openai/dart_openai.dart';
 import 'package:flutter/material.dart' hide AppBar;
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_screwdriver/flutter_screwdriver.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -20,7 +19,7 @@ class NoteView extends StatelessWidget {
     final NoteModel noteModel =
         ModalRoute.of(context)!.settings.arguments as NoteModel;
     const String model = 'text-davinci-003';
-    const List<String> labels = [
+    final List<String> labels = [
       'Very Unpleasant',
       'Unpleasant',
       'Slightly Unpleasant',
@@ -29,26 +28,6 @@ class NoteView extends StatelessWidget {
       'Pleasant',
       'Very Pleasant'
     ];
-    OpenAI.instance.completion
-        .create(
-      model: model,
-      prompt: buildPrompt(noteModel, labels),
-      temperature: 0,
-      maxTokens: 60,
-      topP: 1,
-      frequencyPenalty: 0.5,
-      presencePenalty: 0,
-      n: 1,
-      stop: 'Label:',
-      echo: true,
-    )
-        .then((OpenAICompletionModel completion) {
-      log(
-        completion.choices.first.text.split('Label:').last.trim(),
-        name:
-            'NoteView:build:completion.choices.first.text.split(\'Label:\').last.trim()',
-      );
-    });
     return BlueprintView(
       appBar: const AppBar(
         automaticallyImplyLeading: true,
@@ -143,6 +122,57 @@ class NoteView extends StatelessWidget {
                 color: context.theme.colorScheme.onBackground,
               ),
             ),
+          ),
+          SizedBox(height: gap),
+          FutureBuilder(
+            future: OpenAI.instance.completion.create(
+              model: model,
+              prompt: buildPrompt(noteModel, labels),
+              temperature: 0,
+              maxTokens: 60,
+              topP: 1,
+              frequencyPenalty: 0.5,
+              presencePenalty: 0,
+              n: 1,
+              stop: 'Label:',
+              echo: true,
+            ),
+            builder: (
+              BuildContext context,
+              AsyncSnapshot<OpenAICompletionModel>
+                  openAICompletionModelSnapshot,
+            ) {
+              String sentiment = '';
+              if (openAICompletionModelSnapshot.connectionState ==
+                  ConnectionState.done) {
+                sentiment = openAICompletionModelSnapshot
+                    .data!.choices.first.text
+                    .split('Label:')
+                    .last
+                    .trim();
+                return CustomListTile(
+                  titleString: 'Sentiment',
+                  contentPadding: EdgeInsets.all(gap),
+                  subtitle: SelectableText(
+                    sentiment,
+                    style: context.theme.textTheme.bodyLarge!.copyWith(
+                      color: context.theme.colorScheme.onBackground,
+                    ),
+                  ),
+                ).animate().fade().shimmer();
+              } else {
+                return CustomListTile(
+                  titleString: 'Sentiment',
+                  contentPadding: EdgeInsets.all(gap),
+                  subtitle: SelectableText(
+                    sentiment,
+                    style: context.theme.textTheme.bodyLarge!.copyWith(
+                      color: context.theme.colorScheme.onBackground,
+                    ),
+                  ),
+                ).animate().fade().shimmer();
+              }
+            },
           ),
         ],
       ),
