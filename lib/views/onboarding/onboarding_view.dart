6e0ecdeccea9a043dc162b8121adacc08b04795e
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-09-04 01:15:05 +0300

    improves onboarding with better ui and smoother animations

--- lib/views/onboarding/onboarding_view.dart
+++ lib/views/onboarding/onboarding_view.dart
@@ -2,24 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screwdriver/flutter_screwdriver.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-import 'package:sprung/sprung.dart';
 
 import '../../controllers/onboarding/onboarding_controller.dart';
 import '../../utils/constants.dart';
 import '../blueprint/blueprint_view.dart';
 import 'widgets/onboarding_navigation_bar.dart';
 
-class OnboardingView extends ConsumerStatefulWidget {
+class OnboardingView extends ConsumerWidget {
   const OnboardingView({super.key});
 
   @override
-  ConsumerState<OnboardingView> createState() => _OnboardingViewState();
-}
-
-class _OnboardingViewState extends ConsumerState<OnboardingView> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     ref.watch(OnboardingController().pageControllerProvider).addListener(() {
       ref
               .watch(OnboardingController().currentPageIndexProvider.notifier)
@@ -31,77 +26,100 @@ class _OnboardingViewState extends ConsumerState<OnboardingView> {
     });
     return BlueprintView(
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: gap),
+        padding: EdgeInsets.only(top: gap),
         child: Column(
           mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Expanded(
               child: PageView.builder(
+                clipBehavior: Clip.none,
                 controller:
                     ref.watch(OnboardingController().pageControllerProvider),
                 physics: const NeverScrollableScrollPhysics(),
                 itemCount: OnboardingController().onboardingScreenViews.length,
                 // depending on the current index, animate slide from left or right and opacity
-                itemBuilder: (_, int index) => OnboardingController()
-                    .currentOnboardingScreenView(index)
-                    .animate()
-                    .fadeIn(
-                      curve: curve,
-                      duration: 1.5.seconds,
+                itemBuilder: (context, index) {
+                  final bool active = index ==
+                      ref.watch(
+                          OnboardingController().currentPageIndexProvider);
+                  final double opacity = active ? 1 : 0;
+                  final double slide = active
+                      ? 0
+                      : index <
+                              ref.watch(OnboardingController()
+                                  .currentPageIndexProvider)
+                          ? -1
+                          : 1;
+                  return AnimatedContainer(
+                    duration: emphasizedDuration,
+                    curve: emphasizedCurve,
+                    transform: Matrix4.identity()
+                      ..translate(slide * 100.w)
+                      ..scale(active ? 1.0 : 0.9),
+                    child: AnimatedOpacity(
+                      duration: emphasizedDuration,
+                      curve: emphasizedCurve,
+                      opacity: opacity,
+                      child: OnboardingController()
+                          .currentOnboardingScreenView(index),
                     ),
-              ),
-            )
-                .animate()
-                .fade(
-                  begin: -1,
-                  duration: 1.5.seconds,
-                  curve: Sprung.overDamped,
-                )
-                .slideY(
-                  begin: -0.5,
-                  end: 0,
-                  duration: 1.5.seconds,
-                  curve: Sprung.overDamped,
+                  );
+                },
+              )
+                  .animate()
+                  .fade(
+                    begin: -1,
+                    duration: emphasizedDuration,
+                    curve: emphasizedCurve,
+                  )
+                  .slideY(
+                    begin: -0.5,
+                    end: 0,
+                    duration: emphasizedDuration,
+                    curve: emphasizedCurve,
+                  ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(gap),
+              child: SmoothPageIndicator(
+                controller:
+                    ref.watch(OnboardingController().pageControllerProvider),
+                count: OnboardingController().onboardingScreenViews.length,
+                effect: ExpandingDotsEffect(
+                  activeDotColor: context.theme.colorScheme.primary,
+                  dotColor: context.theme.colorScheme.surfaceVariant,
                 ),
-            SmoothPageIndicator(
-              controller:
-                  ref.watch(OnboardingController().pageControllerProvider),
-              count: OnboardingController().onboardingScreenViews.length,
-              effect: ExpandingDotsEffect(
-                activeDotColor: theme.colorScheme.primary,
-                dotColor: theme.colorScheme.surfaceVariant,
-              ),
-            )
-                .animate(
-                  delay: 1.5.seconds,
-                )
-                .fade(
-                  begin: -1,
-                  duration: 1.5.seconds,
-                  curve: Sprung.overDamped,
-                )
-                .slideY(
-                  begin: 1,
-                  end: 0,
-                  duration: 1.5.seconds,
-                  curve: Sprung.overDamped,
-                ),
-            SizedBox(height: gap * 2),
+              )
+                  .animate(
+                    delay: 1.5.seconds,
+                  )
+                  .fade(
+                    begin: -1,
+                    duration: emphasizedDuration,
+                    curve: emphasizedCurve,
+                  )
+                  .slideY(
+                    begin: 1,
+                    end: 0,
+                    duration: emphasizedDuration,
+                    curve: emphasizedCurve,
+                  ),
+            ),
             const OnboardingNavigationBar()
                 .animate(
                   delay: 1.seconds,
                 )
                 .fade(
                   begin: -1,
-                  duration: 1.5.seconds,
-                  curve: Sprung.overDamped,
+                  duration: emphasizedDuration,
+                  curve: emphasizedCurve,
                 )
                 .slideY(
                   begin: 1,
                   end: 0,
-                  duration: 1.5.seconds,
-                  curve: Sprung.overDamped,
+                  duration: emphasizedDuration,
+                  curve: emphasizedCurve,
                 ),
           ],
         ),
