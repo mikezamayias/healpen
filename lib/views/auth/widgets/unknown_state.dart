cfdd2d7b529971b350940662c55392675f06483c
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-09-04 00:23:19 +0300

    improves unknown state ui

--- lib/views/auth/widgets/unknown_state.dart
+++ lib/views/auth/widgets/unknown_state.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screwdriver/flutter_screwdriver.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import '../../../utils/constants.dart';
 import '../../../widgets/custom_list_tile.dart';
 
 class UnknownState extends StatelessWidget {
@@ -10,9 +13,30 @@ class UnknownState extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomListTile(
-      titleString: 'Unknown state',
-      subtitleString: '${state.runtimeType}',
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CustomListTile(
+          leadingIconData: FontAwesomeIcons.solidCircleQuestion,
+          selectableText: true,
+          backgroundColor: context.theme.colorScheme.tertiary,
+          textColor: context.theme.colorScheme.onTertiary,
+          titleString: 'Unknown state',
+          subtitleString: '${state.runtimeType}',
+        ),
+        SizedBox(height: gap),
+        CustomListTile(
+          responsiveWidth: true,
+          leadingIconData: FontAwesomeIcons.arrowLeft,
+          selectableText: true,
+          // backgroundColor: context.theme.colorScheme.error,
+          // textColor: context.theme.colorScheme.onError,
+          titleString: 'Go back',
+          onTap: () => context.navigator.pushReplacementNamed('/auth'),
+        ),
+      ],
     );
   }
 }
