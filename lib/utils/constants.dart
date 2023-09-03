7bc13452002538683ee67f10956a9cec22da83be
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-09-04 00:21:55 +0300

    simplifies animation duration and curve

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
-import 'package:sprung/sprung.dart';
 
 final double unit = 4.2.sp;
 final double gap = 9 * unit;
 final double radius = 30 - gap;
 final double elevation = 0 * unit;
-const int animationDuration = 150;
-final Curve curve = Sprung.overDamped;
+const Curve curve = Cubic(0.2, 0.0, 0, 1.0);
 final scaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
