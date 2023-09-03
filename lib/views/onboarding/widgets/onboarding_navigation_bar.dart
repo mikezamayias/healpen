286be1e98df09be14ebeb62e5a7a06c2ceab971a
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-09-04 01:14:37 +0300

    refactors onboarding navigation bar

--- lib/views/onboarding/widgets/onboarding_navigation_bar.dart
+++ lib/views/onboarding/widgets/onboarding_navigation_bar.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screwdriver/flutter_screwdriver.dart';
 
 import '../../../controllers/onboarding/onboarding_controller.dart';
 import '../../../controllers/settings/preferences_controller.dart';
 import '../../../utils/constants.dart';
+import '../../auth/auth_view.dart';
 import 'onboarding_button.dart';
 
 class OnboardingNavigationBar extends ConsumerStatefulWidget {
@@ -19,13 +20,46 @@ class OnboardingNavigationBar extends ConsumerStatefulWidget {
 class _OnboardingNavigationBarState
     extends ConsumerState<OnboardingNavigationBar> {
   void goToAuth() {
-    ref
-        .watch(OnboardingController().onboardingCompletedProvider.notifier)
-        .state = true;
-    PreferencesController()
-        .onboardingCompleted
-        .write(ref.watch(OnboardingController().onboardingCompletedProvider));
-    navigator.pushNamed('/auth');
+    HapticFeedback.vibrate().whenComplete(() {
+      ref
+          .watch(OnboardingController().onboardingCompletedProvider.notifier)
+          .state = true;
+      PreferencesController()
+          .onboardingCompleted
+          .write(ref.watch(OnboardingController().onboardingCompletedProvider));
+      navigator.push(
+        PageRouteBuilder(
+          transitionDuration: emphasizedDuration,
+          reverseTransitionDuration: emphasizedDuration,
+          pageBuilder: (context, animation, secondaryAnimation) =>
+              const AuthView(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+              FadeTransition(
+            opacity: Tween<double>(
+              begin: -1,
+              end: 1,
+            ).animate(animation),
+            child: SlideTransition(
+              position: Tween<Offset>(
+                begin: const Offset(1, 0),
+                end: Offset.zero,
+              ).animate(animation),
+              child: child,
+            ),
+          ),
+        ),
+      );
+    });
+  }
+
+  void goToPage(int index) {
+    HapticFeedback.vibrate().whenComplete(() {
+      ref.watch(OnboardingController().pageControllerProvider).animateToPage(
+            index,
+            duration: emphasizedDuration,
+            curve: emphasizedCurve,
+          );
+    });
   }
 
   @override
@@ -33,47 +67,141 @@ class _OnboardingNavigationBarState
     int currentPageIndex = ref.watch(
       OnboardingController().currentPageIndexProvider,
     );
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        if (currentPageIndex == 0)
-          OnboardingButton(
-            titleString: 'Skip',
-            onTap: goToAuth,
-          )
-        else
-          OnboardingButton(
-            titleString: 'Back',
-            onTap: () {
-              ref
-                  .watch(OnboardingController().pageControllerProvider)
-                  .animateToPage(
-                    currentPageIndex - 1,
-                    duration: animationDuration.milliseconds,
-                    curve: curve,
-                  );
-            },
-          ),
-        if (currentPageIndex ==
-            OnboardingController().onboardingScreenViews.length - 1)
-          OnboardingButton(
-            titleString: 'Start Writing Now',
-            onTap: goToAuth,
-          )
-        else
-          OnboardingButton(
-            titleString: currentPageIndex == 0 ? 'Get Started' : 'Next',
-            onTap: () {
-              ref
-                  .watch(OnboardingController().pageControllerProvider)
-                  .animateToPage(
-                    currentPageIndex + 1,
-                    duration: animationDuration.milliseconds,
-                    curve: curve,
-                  );
-            },
-          )
-      ],
+    return Padding(
+      padding: EdgeInsets.all(gap),
+      child: AnimatedSwitcher(
+        duration: emphasizedDuration,
+        reverseDuration: standardDuration,
+        switchInCurve: standardCurve,
+        switchOutCurve: standardCurve,
+        transitionBuilder: (child, animation) => FadeTransition(
+          opacity: animation,
+          child: child,
+        ),
+        child: Row(
+          key: currentPageIndex == 0 ||
+                  currentPageIndex ==
+                      OnboardingController().onboardingScreenViews.length - 1
+              ? ValueKey<int>(currentPageIndex)
+              : null,
+          // Key ensures a new widget is detected
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            if (currentPageIndex == 0) ...[
+              OnboardingButton(
+                titleString: 'Skip',
+                onTap: goToAuth,
+              ),
+              OnboardingButton(
+                titleString: 'Get Started',
+                onTap: () {
+                  goToPage(currentPageIndex + 1);
+                },
+              ),
+            ] else if (currentPageIndex ==
+                OnboardingController().onboardingScreenViews.length - 1) ...[
+              OnboardingButton(
+                titleString: 'Back',
+                onTap: () {
+                  goToPage(currentPageIndex - 1);
+                },
+              ),
+              OnboardingButton(
+                titleString: 'Start Writing Now',
+                onTap: goToAuth,
+              ),
+            ] else ...[
+              OnboardingButton(
+                titleString: 'Back',
+                onTap: () {
+                  goToPage(currentPageIndex - 1);
+                },
+              ),
+              OnboardingButton(
+                titleString: 'Next',
+                onTap: () {
+                  goToPage(currentPageIndex + 1);
+                },
+              ),
+            ],
+          ],
+        ),
+      ),
     );
+
+    // return Row(
+    //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //   children: [
+    //     // First Button CrossFade (Skip <-> Back)
+    //     ClipRRect(
+    //       borderRadius: BorderRadius.circular(radius),
+    //       child: AnimatedCrossFade(
+    //         duration: 50.milliseconds,
+    //         reverseDuration: 50.milliseconds,
+    //         sizeCurve: standardCurve,
+    //         firstCurve: standardCurve,
+    //         secondCurve: standardCurve,
+    //         firstChild: OnboardingButton(
+    //           titleString: 'Skip',
+    //           onTap: goToAuth,
+    //         ),
+    //         secondChild: OnboardingButton(
+    //           titleString: 'Back',
+    //           onTap: () {
+    //             goToPage(currentPageIndex - 1);
+    //           },
+    //         ),
+    //         crossFadeState: currentPageIndex == 0
+    //             ? CrossFadeState.showFirst
+    //             : CrossFadeState.showSecond,
+    //       ),
+    //     ),
+    //
+    //     // Second Button CrossFade
+    //     ClipRRect(
+    //       borderRadius: BorderRadius.circular(radius),
+    //       child: AnimatedCrossFade(
+    //         duration: 50.milliseconds,
+    //         reverseDuration: 50.milliseconds,
+    //         sizeCurve: standardCurve,
+    //         firstCurve: standardCurve,
+    //         secondCurve: standardCurve,
+    //         firstChild: OnboardingButton(
+    //           titleString: 'Get Started',
+    //           onTap: () {
+    //             goToPage(currentPageIndex + 1);
+    //           },
+    //         ),
+    //         secondChild: ClipRRect(
+    //           borderRadius: BorderRadius.circular(radius),
+    //           child: AnimatedCrossFade(
+    //             duration: 50.milliseconds,
+    //             reverseDuration: 50.milliseconds,
+    //             sizeCurve: standardCurve,
+    //             firstCurve: standardCurve,
+    //             secondCurve: standardCurve,
+    //             firstChild: OnboardingButton(
+    //               titleString: 'Next',
+    //               onTap: () {
+    //                 goToPage(currentPageIndex + 1);
+    //               },
+    //             ),
+    //             secondChild: OnboardingButton(
+    //               titleString: 'Start Writing Now',
+    //               onTap: goToAuth,
+    //             ),
+    //             crossFadeState: currentPageIndex ==
+    //                     OnboardingController().onboardingScreenViews.length - 1
+    //                 ? CrossFadeState.showSecond
+    //                 : CrossFadeState.showFirst,
+    //           ),
+    //         ),
+    //         crossFadeState: currentPageIndex == 0
+    //             ? CrossFadeState.showFirst
+    //             : CrossFadeState.showSecond,
+    //       ),
+    //     ),
+    //   ],
+    // );
   }
 }
