d85faf34c2ee35657b341a6b50004f59ae54e95e
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-09-04 13:10:20 +0300

    refactors

--- lib/widgets/custom_chip.dart
+++ lib/widgets/custom_chip.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_screwdriver/flutter_screwdriver.dart';
 
 import '../utils/constants.dart';
