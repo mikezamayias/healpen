20dafc5d6f14fa06de96aec626e7ef13c6dc84e3
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-09-07 12:42:07 +0300

    adds vibration on show dialog

--- lib/utils/show_healpen_dialog.dart
+++ lib/utils/show_healpen_dialog.dart
@@ -1,24 +1,32 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screwdriver/flutter_screwdriver.dart';
 
+import '../providers/settings_providers.dart';
 import '../widgets/custom_dialog.dart';
 import 'constants.dart';
+import 'helper_functions.dart';
 
 Future showHealpenDialog({
   required BuildContext context,
   required CustomDialog customDialog,
 }) async =>
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      barrierColor: context.theme.colorScheme.background.withOpacity(0.8),
-      builder: (BuildContext context) => BackdropFilter(
-        filter: ImageFilter.blur(
-          sigmaX: gap * 2,
-          sigmaY: gap * 2,
-        ),
-        child: customDialog,
-      ),
+    vibrate(
+      ProviderContainer().read(navigationReduceHapticFeedbackProvider),
+      () async {
+        showDialog(
+          context: context,
+          barrierDismissible: false,
+          barrierColor: context.theme.colorScheme.background.withOpacity(0.8),
+          builder: (BuildContext context) => BackdropFilter(
+            filter: ImageFilter.blur(
+              sigmaX: gap * 2,
+              sigmaY: gap * 2,
+            ),
+            child: customDialog,
+          ),
+        );
+      },
     );
