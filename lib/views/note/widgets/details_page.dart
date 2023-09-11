89f359d5a88c7d41affd85c988445f5f04acf55a
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-09-11 16:49:12 +0300

    fixes overflow error

--- lib/views/note/widgets/details_page.dart
+++ lib/views/note/widgets/details_page.dart
@@ -20,94 +20,101 @@ class DetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: gap),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: gap),
-            child: const TextDivider('Details'),
-          ),
-          Row(
-            children: [
-              Expanded(
-                flex: 2,
-                child: CustomListTile(
-                  titleString: 'Date and Time',
-                  responsiveWidth: true,
-                  contentPadding: EdgeInsets.all(gap),
-                  subtitle: SelectableText(
-                    noteModel.timestamp.timestampFormat(),
-                    style: context.theme.textTheme.bodyLarge!.copyWith(
-                      color: context.theme.colorScheme.onBackground,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(radius),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: gap),
+                child: const TextDivider('Details'),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: CustomListTile(
+                      titleString: 'Date and Time',
+                      responsiveWidth: true,
+                      contentPadding: EdgeInsets.all(gap),
+                      subtitle: SelectableText(
+                        noteModel.timestamp.timestampFormat(),
+                        style: context.theme.textTheme.bodyLarge!.copyWith(
+                          color: context.theme.colorScheme.onBackground,
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                  SizedBox(width: gap),
+                  Expanded(
+                    child: CustomListTile(
+                      responsiveWidth: true,
+                      contentPadding: EdgeInsets.all(gap),
+                      titleString: 'Duration',
+                      subtitle: Text(
+                        noteModel.duration.writingDurationFormat(),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(width: gap),
-              Expanded(
-                child: CustomListTile(
-                  responsiveWidth: true,
-                  contentPadding: EdgeInsets.all(gap),
-                  titleString: 'Duration',
-                  subtitle: Text(
-                    noteModel.duration.writingDurationFormat(),
+              SizedBox(height: gap),
+              Row(
+                children: [
+                  Expanded(
+                    child: CustomListTile(
+                      responsiveWidth: true,
+                      contentPadding: EdgeInsets.all(gap),
+                      titleString: 'Private',
+                      subtitle: Center(
+                        child: FaIcon(
+                          noteModel.isPrivate
+                              ? FontAwesomeIcons.lock
+                              : FontAwesomeIcons.lockOpen,
+                          size: context
+                                  .theme.textTheme.headlineSmall!.fontSize! +
+                              context.theme.textTheme.headlineSmall!.height!,
+                          color: context.theme.colorScheme.onBackground,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: gap),
+                  Expanded(
+                    child: CustomListTile(
+                      responsiveWidth: true,
+                      contentPadding: EdgeInsets.all(gap),
+                      titleString: 'Favorite',
+                      subtitle: Center(
+                        child: FaIcon(
+                          noteModel.isFavorite
+                              ? FontAwesomeIcons.solidStar
+                              : FontAwesomeIcons.star,
+                          size: context
+                                  .theme.textTheme.headlineSmall!.fontSize! +
+                              context.theme.textTheme.headlineSmall!.height!,
+                          color: context.theme.colorScheme.onBackground,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: gap),
+              CustomListTile(
+                titleString: 'Content',
+                contentPadding: EdgeInsets.all(gap),
+                subtitle: SelectableText(
+                  noteModel.content,
+                  style: context.theme.textTheme.bodyLarge!.copyWith(
+                    color: context.theme.colorScheme.onBackground,
                   ),
                 ),
               ),
             ],
           ),
-          SizedBox(height: gap),
-          Row(
-            children: [
-              Expanded(
-                child: CustomListTile(
-                  responsiveWidth: true,
-                  contentPadding: EdgeInsets.all(gap),
-                  titleString: 'Private',
-                  subtitle: Center(
-                    child: FaIcon(
-                      noteModel.isPrivate
-                          ? FontAwesomeIcons.lock
-                          : FontAwesomeIcons.lockOpen,
-                      size: context.theme.textTheme.headlineSmall!.fontSize! +
-                          context.theme.textTheme.headlineSmall!.height!,
-                      color: context.theme.colorScheme.onBackground,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(width: gap),
-              Expanded(
-                child: CustomListTile(
-                  responsiveWidth: true,
-                  contentPadding: EdgeInsets.all(gap),
-                  titleString: 'Favorite',
-                  subtitle: Center(
-                    child: FaIcon(
-                      noteModel.isFavorite
-                          ? FontAwesomeIcons.solidStar
-                          : FontAwesomeIcons.star,
-                      size: context.theme.textTheme.headlineSmall!.fontSize! +
-                          context.theme.textTheme.headlineSmall!.height!,
-                      color: context.theme.colorScheme.onBackground,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: gap),
-          CustomListTile(
-            titleString: 'Content',
-            contentPadding: EdgeInsets.all(gap),
-            subtitle: SelectableText(
-              noteModel.content,
-              style: context.theme.textTheme.bodyLarge!.copyWith(
-                color: context.theme.colorScheme.onBackground,
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
