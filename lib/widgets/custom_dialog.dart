14230799b1583c3d62003bf88f0dfdec5475876f
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-09-12 12:11:51 +0300

    refactors dialog

--- lib/widgets/custom_dialog.dart
+++ lib/widgets/custom_dialog.dart
@@ -1,14 +1,16 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screwdriver/flutter_screwdriver.dart';
-import 'package:responsive_sizer/responsive_sizer.dart';
 
 import '../utils/constants.dart';
+import 'custom_list_tile.dart';
 
 class CustomDialog extends StatelessWidget {
   final String titleString;
   final String? contentString;
   final Color? backgroundColor;
-  final List<Widget> actions;
+  final List<CustomListTile> actions;
 
   const CustomDialog({
     super.key,
@@ -20,58 +22,70 @@ class CustomDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      iconPadding: EdgeInsets.zero,
-      insetPadding: EdgeInsets.zero,
-      buttonPadding: EdgeInsets.zero,
-      contentPadding: EdgeInsets.zero,
-      titlePadding: EdgeInsets.all(gap),
-      actionsPadding: EdgeInsets.only(
-        left: gap,
-        right: gap,
-        bottom: gap,
+    return BackdropFilter(
+      filter: ImageFilter.blur(
+        sigmaX: gap * 2,
+        sigmaY: gap * 2,
       ),
-      backgroundColor:
-          backgroundColor ?? context.theme.colorScheme.primaryContainer,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(radius),
-      ),
-      title: SizedBox(
-        width: 75.w,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              titleString,
-              style: context.theme.textTheme.headlineSmall!.copyWith(
-                color: context.theme.colorScheme.onPrimaryContainer,
-              ),
-              textAlign: TextAlign.start,
-            ),
-            if (contentString != null) ...[
-              SizedBox(height: gap),
-              Container(
-                padding: EdgeInsets.all(gap),
-                decoration: BoxDecoration(
-                  color: context.theme.colorScheme.background,
-                  borderRadius: BorderRadius.circular(radius - gap),
+      child: Dialog(
+        insetPadding: EdgeInsets.all(gap * 2),
+        elevation: 0,
+        backgroundColor:
+            backgroundColor ?? context.theme.colorScheme.primaryContainer,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(radius),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(gap),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                titleString,
+                style: context.theme.textTheme.headlineSmall!.copyWith(
+                  color: context.theme.colorScheme.onPrimaryContainer,
                 ),
-                child: Text(
-                  contentString!,
-                  style: context.theme.textTheme.bodyLarge!.copyWith(
-                    color: context.theme.colorScheme.onBackground,
+                textAlign: TextAlign.start,
+              ),
+              if (contentString != null) ...[
+                SizedBox(height: gap),
+                Container(
+                  padding: EdgeInsets.all(gap),
+                  decoration: BoxDecoration(
+                    color: context.theme.colorScheme.background,
+                    borderRadius: BorderRadius.circular(radius - gap),
                   ),
-                  textAlign: TextAlign.start,
+                  child: Text(
+                    contentString!,
+                    style: context.theme.textTheme.bodyLarge!.copyWith(
+                      color: context.theme.colorScheme.onBackground,
+                    ),
+                    textAlign: TextAlign.start,
+                  ),
                 ),
-              ),
+              ],
+              if (actions.isNotEmpty) ...[
+                Padding(
+                  padding: EdgeInsets.only(top: gap),
+                  child: Row(
+                    // add a sized box between children to add space between them
+                    children: [
+                      for (int i = 0; i < actions.length; i++) ...[
+                        actions[i],
+                        if (i != actions.length - 1) ...[
+                          SizedBox(width: gap),
+                        ],
+                      ],
+                    ],
+                  ),
+                ),
+              ],
             ],
-          ],
+          ),
         ),
       ),
-      actionsAlignment: MainAxisAlignment.end,
-      actions: actions,
     );
   }
 }
