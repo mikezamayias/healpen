2c2bbcc7bea52fd88a59fbadfb76f935b9dad13f
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-09-20 14:58:23 +0300

    adds a slider to show the average overall sentiment in range

--- lib/views/analysis/analysis_view.dart
+++ lib/views/analysis/analysis_view.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart' hide AppBar;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screwdriver/flutter_screwdriver.dart';
@@ -8,6 +9,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../../controllers/analysis_view_controller.dart';
 import '../../models/note/note_model.dart';
 import '../../providers/settings_providers.dart';
+import '../../utils/constants.dart';
 import '../../utils/show_healpen_dialog.dart';
 import '../../widgets/app_bar.dart';
 import '../../widgets/custom_dialog.dart';
@@ -73,27 +75,44 @@ class AnalysisView extends ConsumerWidget {
               averageSentimentValue += note.sentiment!;
             }
             averageSentimentValue /= metricGroupingsSnapshot.data!.length;
-            return CustomListTile(
-              titleString: 'Average overall sentiment value',
-              leadingIconData: FontAwesomeIcons.circleInfo,
-              leadingOnTap: () {
-                /// explain to the user what they are seeing
-                showHealpenDialog(
-                  context: context,
-                  doVibrate: ref.watch(navigationReduceHapticFeedbackProvider),
-                  customDialog: const CustomDialog(
-                    titleString: 'What is this?',
-                    contentString: 'This is the average sentiment value of all '
-                        'your notes. It is calculated by adding up the '
-                        'sentiment value of each note and dividing it by the '
-                        'number of notes you have written.',
+            return Column(
+              children: [
+                CustomListTile(
+                  titleString: 'Average overall sentiment value',
+                  leadingIconData: FontAwesomeIcons.circleInfo,
+                  leadingOnTap: () {
+                    /// explain to the user what they are seeing
+                    showHealpenDialog(
+                      context: context,
+                      doVibrate:
+                          ref.watch(navigationReduceHapticFeedbackProvider),
+                      customDialog: const CustomDialog(
+                        titleString: 'What is this?',
+                        contentString:
+                            'This is the average sentiment value of all '
+                            'your notes. It is calculated by adding up the '
+                            'sentiment value of each note and dividing it by the '
+                            'number of notes you have written.',
+                      ),
+                    );
+                  },
+                  trailing: Text(
+                    averageSentimentValue.toStringAsFixed(2),
+                    style: context.theme.textTheme.bodyLarge,
                   ),
-                );
-              },
-              trailing: Text(
-                averageSentimentValue.toStringAsFixed(2),
-                style: context.theme.textTheme.bodyLarge,
-              ),
+                ),
+
+                /// show a readonly slider with the average overall sentiment
+                /// value
+                Slider.adaptive(
+                  value: averageSentimentValue,
+                  onChanged: null,
+                  min: sentimentValues.min.toDouble(),
+                  max: sentimentValues.max.toDouble(),
+                  divisions: sentimentValues.length - 1,
+                  label: averageSentimentValue.toStringAsFixed(2),
+                ),
+              ],
             );
           } else {
             return const CustomListTile(
