b109dd256a7a66e405645a4e6561a0088daf0ee5
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-09-25 13:52:48 +0300

    Enables Google API key.

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -6,8 +6,8 @@ part 'env.g.dart';
 abstract class Env {
   @EnviedField(varName: 'OPEN_AI_API_KEY', obfuscate: true)
   static final String openAiApiKey = _Env.openAiApiKey;
-  // @EnviedField(varName: 'GOOGLE_APIS_KEY', obfuscate: true)
-  // static final String googleApisKey = _Env.googleApisKey;
+  @EnviedField(varName: 'GOOGLE_APIS_KEY', obfuscate: true)
+  static final String googleApisKey = _Env.googleApisKey;
   @EnviedField(varName: 'HEALPEN_GITHUB_TOKEN', obfuscate: true)
   static final String healpenGithubToken = _Env.healpenGithubToken;
 }
