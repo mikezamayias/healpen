09ce7c56f5a0d870975daefabdf153fedcef0ab1
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-10-01 19:46:03 +0300

    refactors

--- lib/views/history/widgets/note_tile.dart
+++ lib/views/history/widgets/note_tile.dart
@@ -9,10 +9,7 @@ import '../../../models/note/note_model.dart';
 import '../../../providers/settings_providers.dart';
 import '../../../utils/constants.dart';
 import '../../../utils/helper_functions.dart';
-import '../../../utils/show_healpen_dialog.dart';
-import '../../../widgets/custom_dialog.dart';
 import '../../../widgets/custom_list_tile.dart';
-import '../../../widgets/custom_snack_bar.dart';
 
 class NoteTile extends ConsumerWidget {
   const NoteTile({
@@ -44,94 +41,95 @@ class NoteTile extends ConsumerWidget {
                 padding: EdgeInsets.all(gap),
                 spacing: gap,
                 onPressed: (context) {
-                  showHealpenDialog(
-                    context: context,
-                    doVibrate:
-                        ref.watch(navigationReduceHapticFeedbackProvider),
-                    customDialog: CustomDialog(
-                      titleString: 'Delete note?',
-                      contentString: 'You cannot undo this action.',
-                      actions: [
-                        CustomListTile(
-                          contentPadding: EdgeInsets.symmetric(
-                            horizontal: gap * 2,
-                            vertical: gap,
-                          ),
-                          cornerRadius: radius - gap,
-                          responsiveWidth: true,
-                          titleString: 'Yes',
-                          onTap: () {
-                            vibrate(
-                              ref.watch(navigationReduceHapticFeedbackProvider),
-                              () {
-                                navigatorKey.currentState?.pop(true);
-                              },
-                            );
-                          },
-                        ),
-                        CustomListTile(
-                          contentPadding: EdgeInsets.symmetric(
-                            horizontal: gap * 2,
-                            vertical: gap,
-                          ),
-                          cornerRadius: radius - gap,
-                          responsiveWidth: true,
-                          titleString: 'No',
-                          onTap: () {
-                            vibrate(
-                              ref.watch(navigationReduceHapticFeedbackProvider),
-                              () {
-                                navigatorKey.currentState?.pop(false);
-                              },
-                            );
-                          },
-                        ),
-                      ],
-                    ),
-                  ).then(
-                    (exit) {
-                      if (exit == null) return;
-                      if (exit) {
-                        CustomSnackBar(
-                          SnackBarConfig(
-                            vibrate: ref
-                                .watch(navigationReduceHapticFeedbackProvider),
-                            titleString1: 'Deleting note...',
-                            leadingIconData1: FontAwesomeIcons.trashCan,
-                            actionAfterSnackBar1: () async =>
-                                HistoryViewController()
-                                    .deleteNote(noteModel: entry),
-                            trailingWidgets1: [
-                              CustomListTile(
-                                contentPadding: EdgeInsets.symmetric(
-                                  horizontal: gap * 2,
-                                  vertical: gap,
-                                ),
-                                cornerRadius: radius - gap,
-                                responsiveWidth: true,
-                                backgroundColor:
-                                    context.theme.colorScheme.primaryContainer,
-                                textColor: context
-                                    .theme.colorScheme.onPrimaryContainer,
-                                onTap: () {
-                                  vibrate(
-                                    ref.watch(
-                                        navigationReduceHapticFeedbackProvider),
-                                    () {
-                                      scaffoldMessengerKey
-                                          .currentState!.removeCurrentSnackBar;
-                                    },
-                                  );
-                                },
-                                titleString: 'Cancel',
-                                leadingIconData: FontAwesomeIcons.xmark,
-                              ),
-                            ],
-                          ),
-                        ).showSnackBar(context);
-                      }
-                    },
-                  );
+                  HistoryViewController().deleteNote(noteModel: entry);
+                  // showHealpenDialog(
+                  //   context: context,
+                  //   doVibrate:
+                  //       ref.watch(navigationReduceHapticFeedbackProvider),
+                  //   customDialog: CustomDialog(
+                  //     titleString: 'Delete note?',
+                  //     contentString: 'You cannot undo this action.',
+                  //     actions: [
+                  //       CustomListTile(
+                  //         contentPadding: EdgeInsets.symmetric(
+                  //           horizontal: gap * 2,
+                  //           vertical: gap,
+                  //         ),
+                  //         cornerRadius: radius - gap,
+                  //         responsiveWidth: true,
+                  //         titleString: 'Yes',
+                  //         onTap: () {
+                  //           vibrate(
+                  //             ref.watch(navigationReduceHapticFeedbackProvider),
+                  //             () {
+                  //               navigatorKey.currentState?.pop(true);
+                  //             },
+                  //           );
+                  //         },
+                  //       ),
+                  //       CustomListTile(
+                  //         contentPadding: EdgeInsets.symmetric(
+                  //           horizontal: gap * 2,
+                  //           vertical: gap,
+                  //         ),
+                  //         cornerRadius: radius - gap,
+                  //         responsiveWidth: true,
+                  //         titleString: 'No',
+                  //         onTap: () {
+                  //           vibrate(
+                  //             ref.watch(navigationReduceHapticFeedbackProvider),
+                  //             () {
+                  //               navigatorKey.currentState?.pop(false);
+                  //             },
+                  //           );
+                  //         },
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ).then(
+                  //   (exit) {
+                  //     if (exit == null) return;
+                  //     if (exit) {
+                  //       CustomSnackBar(
+                  //         SnackBarConfig(
+                  //           vibrate: ref
+                  //               .watch(navigationReduceHapticFeedbackProvider),
+                  //           titleString1: 'Deleting note...',
+                  //           leadingIconData1: FontAwesomeIcons.trashCan,
+                  //           actionAfterSnackBar1: () async =>
+                  //               HistoryViewController()
+                  //                   .deleteNote(noteModel: entry),
+                  //           trailingWidgets1: [
+                  //             CustomListTile(
+                  //               contentPadding: EdgeInsets.symmetric(
+                  //                 horizontal: gap * 2,
+                  //                 vertical: gap,
+                  //               ),
+                  //               cornerRadius: radius - gap,
+                  //               responsiveWidth: true,
+                  //               backgroundColor:
+                  //                   context.theme.colorScheme.primaryContainer,
+                  //               textColor: context
+                  //                   .theme.colorScheme.onPrimaryContainer,
+                  //               onTap: () {
+                  //                 vibrate(
+                  //                   ref.watch(
+                  //                       navigationReduceHapticFeedbackProvider),
+                  //                   () {
+                  //                     scaffoldMessengerKey
+                  //                         .currentState!.removeCurrentSnackBar;
+                  //                   },
+                  //                 );
+                  //               },
+                  //               titleString: 'Cancel',
+                  //               leadingIconData: FontAwesomeIcons.xmark,
+                  //             ),
+                  //           ],
+                  //         ),
+                  //       ).showSnackBar(context);
+                  //     }
+                  //   },
+                  // );
                 },
               ),
             ],
