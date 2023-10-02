3be2b9be2a1f7ef82b3f3d7e4931569c95f43c17
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-10-02 16:18:43 +0300

    adds circle pulses

--- lib/views/analysis/widgets/mood_exploration/emotional_echo_tile.dart
+++ lib/views/analysis/widgets/mood_exploration/emotional_echo_tile.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screwdriver/flutter_screwdriver.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+import '../../../../utils/constants.dart';
 
 class EmotionalEchoTile extends ConsumerWidget {
   const EmotionalEchoTile({
@@ -8,6 +13,144 @@ class EmotionalEchoTile extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Text('Emotional Echo');
+    return Center(
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            RepaintBoundary(
+              child: Container(
+                margin: EdgeInsets.zero,
+                child: Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.circular(100.w),
+                    border: Border.all(
+                      color:
+                          context.theme.colorScheme.primary.withOpacity(0.25),
+                      width: gap * 4,
+                    ),
+                  ),
+                )
+                    .animate(
+                      delay: standardDuration,
+                      onPlay: (animationController) =>
+                          animationController.loop(),
+                    )
+                    .scaleXY(
+                      begin: 0.9,
+                      duration: emphasizedDuration,
+                      curve: emphasizedCurve,
+                    )
+                    .then(duration: longEmphasizedDuration)
+                    .scaleXY(end: 0.9),
+              ),
+            ),
+            RepaintBoundary(
+              child: Container(
+                margin: EdgeInsets.all(gap),
+                child: Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.circular(100.w),
+                    border: Border.all(
+                      color:
+                          context.theme.colorScheme.primary.withOpacity(0.25),
+                      width: gap * 3,
+                    ),
+                  ),
+                )
+                    .animate(
+                      delay: standardDuration,
+                      onPlay: (animationController) =>
+                          animationController.loop(),
+                    )
+                    .scaleXY(
+                      begin: 0.9,
+                      duration: emphasizedDuration,
+                      curve: emphasizedCurve,
+                    )
+                    .then(duration: longEmphasizedDuration)
+                    .scaleXY(end: 0.9),
+              ),
+            ),
+            RepaintBoundary(
+              child: Container(
+                margin: EdgeInsets.all(gap * 2),
+                child: Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.circular(100.w),
+                    border: Border.all(
+                      color:
+                          context.theme.colorScheme.primary.withOpacity(0.25),
+                      width: gap * 2,
+                    ),
+                  ),
+                )
+                    .animate(
+                      delay: standardDuration,
+                      onPlay: (animationController) =>
+                          animationController.loop(),
+                    )
+                    .scaleXY(
+                      begin: 0.9,
+                      duration: emphasizedDuration,
+                      curve: emphasizedCurve,
+                    )
+                    .then(duration: longEmphasizedDuration)
+                    .scaleXY(end: 0.9),
+              ),
+            ),
+            RepaintBoundary(
+              child: Container(
+                margin: EdgeInsets.all(gap * 3),
+                child: Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: context.theme.colorScheme.primary,
+                    borderRadius: BorderRadius.circular(100.w),
+                    border: Border.all(
+                      color:
+                          context.theme.colorScheme.primary.withOpacity(0.25),
+                      width: gap,
+                    ),
+                  ),
+                )
+                    .animate(
+                      delay: standardDuration,
+                      onPlay: (animationController) =>
+                          animationController.loop(),
+                    )
+                    .scaleXY(
+                      begin: 0.9,
+                      duration: emphasizedDuration,
+                      curve: emphasizedCurve,
+                    )
+                    .then(duration: longEmphasizedDuration)
+                    .scaleXY(end: 0.9),
+              ),
+            ),
+            Text(
+              'sentiment value',
+              style: context.theme.textTheme.titleLarge!.copyWith(
+                color: context.theme.colorScheme.onPrimary,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
+}
+
+class ChartData {
+  ChartData(this.x, this.y);
+
+  final String x;
+  final double y;
 }
