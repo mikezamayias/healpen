bf116ad289cd67f65c5a2cca1221d09a08864db4
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-10-06 15:17:38 +0300

    repalces widgets with rive animation

--- lib/views/analysis/widgets/mood_exploration/emotional_echo_tile.dart
+++ lib/views/analysis/widgets/mood_exploration/emotional_echo_tile.dart
@@ -1,139 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screwdriver/flutter_screwdriver.dart';
-import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:rive/rive.dart';
 
-import '../../../../utils/constants.dart';
-
-class EmotionalEchoTile extends ConsumerWidget {
+class EmotionalEchoTile extends StatelessWidget {
   const EmotionalEchoTile({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return Center(
       child: AspectRatio(
         aspectRatio: 1,
         child: Stack(
           alignment: Alignment.center,
           children: [
-            RepaintBoundary(
-              child: Container(
-                margin: EdgeInsets.zero,
-                child: Container(
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.transparent,
-                    borderRadius: BorderRadius.circular(100.w),
-                    border: Border.all(
-                      color:
-                          context.theme.colorScheme.primary.withOpacity(0.25),
-                      width: gap * 4,
-                    ),
-                  ),
-                )
-                    .animate(
-                      delay: standardDuration,
-                      onPlay: (animationController) =>
-                          animationController.loop(),
-                    )
-                    .scaleXY(
-                      begin: 0.9,
-                      duration: emphasizedDuration,
-                      curve: emphasizedCurve,
-                    )
-                    .then(duration: longEmphasizedDuration)
-                    .scaleXY(end: 0.9),
-              ),
-            ),
-            RepaintBoundary(
-              child: Container(
-                margin: EdgeInsets.all(gap),
-                child: Container(
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.transparent,
-                    borderRadius: BorderRadius.circular(100.w),
-                    border: Border.all(
-                      color:
-                          context.theme.colorScheme.primary.withOpacity(0.25),
-                      width: gap * 3,
-                    ),
-                  ),
-                )
-                    .animate(
-                      delay: standardDuration,
-                      onPlay: (animationController) =>
-                          animationController.loop(),
-                    )
-                    .scaleXY(
-                      begin: 0.9,
-                      duration: emphasizedDuration,
-                      curve: emphasizedCurve,
-                    )
-                    .then(duration: longEmphasizedDuration)
-                    .scaleXY(end: 0.9),
-              ),
-            ),
-            RepaintBoundary(
-              child: Container(
-                margin: EdgeInsets.all(gap * 2),
-                child: Container(
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.transparent,
-                    borderRadius: BorderRadius.circular(100.w),
-                    border: Border.all(
-                      color:
-                          context.theme.colorScheme.primary.withOpacity(0.25),
-                      width: gap * 2,
-                    ),
-                  ),
-                )
-                    .animate(
-                      delay: standardDuration,
-                      onPlay: (animationController) =>
-                          animationController.loop(),
-                    )
-                    .scaleXY(
-                      begin: 0.9,
-                      duration: emphasizedDuration,
-                      curve: emphasizedCurve,
-                    )
-                    .then(duration: longEmphasizedDuration)
-                    .scaleXY(end: 0.9),
-              ),
-            ),
-            RepaintBoundary(
-              child: Container(
-                margin: EdgeInsets.all(gap * 3),
-                child: Container(
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: context.theme.colorScheme.primary,
-                    borderRadius: BorderRadius.circular(100.w),
-                    border: Border.all(
-                      color:
-                          context.theme.colorScheme.primary.withOpacity(0.25),
-                      width: gap,
-                    ),
-                  ),
-                )
-                    .animate(
-                      delay: standardDuration,
-                      onPlay: (animationController) =>
-                          animationController.loop(),
-                    )
-                    .scaleXY(
-                      begin: 0.9,
-                      duration: emphasizedDuration,
-                      curve: emphasizedCurve,
-                    )
-                    .then(duration: longEmphasizedDuration)
-                    .scaleXY(end: 0.9),
-              ),
+            RiveAnimation.asset(
+              'assets/rive/emotional_echo.riv',
+              animations: const ['AllCircles'],
+              fit: BoxFit.fitHeight,
+              onInit: (Artboard artboard) {
+                artboard.forEachComponent(
+                  (child) {
+                    if (child is Shape) {
+                      final Shape shape = child;
+                      shape.fills.first.paint.color =
+                          context.theme.colorScheme.primary.withOpacity(0.25);
+                    }
+                  },
+                );
+              },
             ),
             Text(
               'sentiment value',
@@ -146,11 +42,4 @@ class EmotionalEchoTile extends ConsumerWidget {
       ),
     );
   }
-}
-
-class ChartData {
-  ChartData(this.x, this.y);
-
-  final String x;
-  final double y;
 }
