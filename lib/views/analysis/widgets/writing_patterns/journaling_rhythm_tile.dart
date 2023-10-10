a34736758d38581d65165539a56cc3a2a5bf554e
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-10-10 12:27:37 +0300

    removes calendar view from analysis page

--- lib/views/analysis/widgets/writing_patterns/journaling_rhythm_tile.dart
+++ lib/views/analysis/widgets/writing_patterns/journaling_rhythm_tile.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+import '../../../../widgets/to_be_implemented_tile.dart';
 
 class JournalingRhythmTile extends StatelessWidget {
   const JournalingRhythmTile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SfCalendar(
-      headerDateFormat: 'MMMM d, yyyy',
-      view: CalendarView.month,
-      monthViewSettings: const MonthViewSettings(showAgenda: false),
-    );
+    return const ToBeImplementedTile();
   }
 }
