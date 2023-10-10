b71fe45046b22344b78f32b4b4d5eef29a63ec68
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-10-10 12:26:32 +0300

    adds calendar

--- lib/views/analysis/widgets/writing_patterns/journaling_rhythm_tile.dart
+++ lib/views/analysis/widgets/writing_patterns/journaling_rhythm_tile.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
-
-import '../../../../widgets/to_be_implemented_tile.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class JournalingRhythmTile extends StatelessWidget {
   const JournalingRhythmTile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const ToBeImplementedTile();
+    return SfCalendar(
+      headerDateFormat: 'MMMM d, yyyy',
+      view: CalendarView.month,
+      monthViewSettings: const MonthViewSettings(showAgenda: false),
+    );
   }
 }
