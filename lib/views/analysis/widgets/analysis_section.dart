2f380377378a864297b75856b346f879e069a2ad
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-10-10 12:17:48 +0300

    improves analysis view ui

--- lib/views/analysis/widgets/analysis_section.dart
+++ lib/views/analysis/widgets/analysis_section.dart
@@ -10,6 +10,7 @@ import '../../../../utils/helper_functions.dart';
 import '../../../utils/show_healpen_dialog.dart';
 import '../../../widgets/custom_dialog.dart';
 import '../../../widgets/custom_list_tile.dart';
+import '../../../widgets/text_divider.dart';
 
 class AnalysisSection extends ConsumerStatefulWidget {
   final String sectionName;
@@ -44,83 +45,78 @@ class _AnalysisSectionState extends ConsumerState<AnalysisSection> {
 
   @override
   Widget build(BuildContext context) {
-    return CustomListTile(
-      titleString: widget.sectionName,
-      title: Row(
-        children: [
-          Text(
-            widget.sectionName,
-            style: context.theme.textTheme.titleLarge!.copyWith(
-              color: context.theme.colorScheme.onSurfaceVariant,
-            ),
-          ),
-          const Spacer(),
-          Text(
-            widget.tileData.elementAt(currentPage).titleString,
-            style: TextStyle(
-              color: context.theme.colorScheme.onSurfaceVariant,
-            ),
-          ),
-        ],
-      ),
-      trailing: SmoothPageIndicator(
-        controller: pageController,
-        count: widget.tileData.length,
-        effect: ExpandingDotsEffect(
-          dotHeight: gap,
-          dotWidth: gap,
-          activeDotColor: context.theme.colorScheme.primary,
-          dotColor: context.theme.colorScheme.outline,
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(bottom: gap),
+          child: TextDivider(widget.sectionName),
         ),
-      ),
-      leadingIconData: ref.watch(navigationShowInfoButtonsProvider)
-          ? FontAwesomeIcons.circleInfo
-          : null,
-      leadingOnTap: ref.watch(navigationShowInfoButtonsProvider)
-          ? () {
-              vibrate(
-                ref.watch(navigationEnableHapticFeedbackProvider),
-                () {
-                  showHealpenDialog(
-                    context: context,
-                    doVibrate:
-                        ref.watch(navigationEnableHapticFeedbackProvider),
-                    customDialog: CustomDialog(
-                      titleString: widget.tileData[currentPage].titleString,
-                      contentString:
-                          widget.tileData[currentPage].explanationString,
-                    ),
+        Expanded(
+          child: CustomListTile(
+            titleString: widget.tileData.elementAt(currentPage).titleString,
+            trailing: SmoothPageIndicator(
+              controller: pageController,
+              count: widget.tileData.length,
+              effect: ExpandingDotsEffect(
+                dotHeight: gap,
+                dotWidth: gap,
+                activeDotColor: context.theme.colorScheme.primary,
+                dotColor: context.theme.colorScheme.outline,
+              ),
+            ),
+            leadingIconData: ref.watch(navigationShowInfoButtonsProvider)
+                ? FontAwesomeIcons.circleInfo
+                : null,
+            leadingOnTap: ref.watch(navigationShowInfoButtonsProvider)
+                ? () {
+                    vibrate(
+                      ref.watch(navigationEnableHapticFeedbackProvider),
+                      () {
+                        showHealpenDialog(
+                          context: context,
+                          doVibrate:
+                              ref.watch(navigationEnableHapticFeedbackProvider),
+                          customDialog: CustomDialog(
+                            titleString:
+                                widget.tileData[currentPage].titleString,
+                            contentString:
+                                widget.tileData[currentPage].explanationString,
+                          ),
+                        );
+                      },
+                    );
+                  }
+                : null,
+            enableSubtitleWrapper: false,
+            subtitle: Container(
+              padding: EdgeInsets.symmetric(vertical: gap),
+              decoration: BoxDecoration(
+                color: context.theme.colorScheme.surface,
+                borderRadius: BorderRadius.circular(radius - gap),
+              ),
+              child: PageView.builder(
+                itemCount: widget.tileData.length,
+                itemBuilder: (BuildContext context, int index) => Padding(
+                  padding: EdgeInsets.symmetric(horizontal: gap),
+                  child: widget.tileData[index].content,
+                ),
+                controller: pageController,
+                onPageChanged: (int index) {
+                  vibrate(
+                    ref.watch(navigationEnableHapticFeedbackProvider),
+                    () {
+                      setState(() {
+                        currentPage = index;
+                      });
+                    },
                   );
                 },
-              );
-            }
-          : null,
-      enableSubtitleWrapper: false,
-      subtitle: Container(
-        padding: EdgeInsets.symmetric(vertical: gap),
-        decoration: BoxDecoration(
-          color: context.theme.colorScheme.surface,
-          borderRadius: BorderRadius.circular(radius - gap),
-        ),
-        child: PageView.builder(
-          itemCount: widget.tileData.length,
-          itemBuilder: (BuildContext context, int index) => Padding(
-            padding: EdgeInsets.symmetric(horizontal: gap),
-            child: widget.tileData[index].content,
+              ),
+            ),
           ),
-          controller: pageController,
-          onPageChanged: (int index) {
-            vibrate(
-              ref.watch(navigationEnableHapticFeedbackProvider),
-              () {
-                setState(() {
-                  currentPage = index;
-                });
-              },
-            );
-          },
         ),
-      ),
+      ],
     );
   }
 }
