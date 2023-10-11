1e08d6b740c2be6996039719fc4d30da10d4bd01
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-10-11 14:30:06 +0300

    refactors

--- lib/views/history/widgets/calendar_tile.dart
+++ lib/views/history/widgets/calendar_tile.dart
@@ -1,6 +1,5 @@
 import 'dart:developer';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screwdriver/flutter_screwdriver.dart';
