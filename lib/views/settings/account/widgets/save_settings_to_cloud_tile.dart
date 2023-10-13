3b0887acae405062ee6819c805fa067afd9c7a3f
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-10-13 18:04:05 +0300

    Refactors.

--- lib/views/settings/account/widgets/save_settings_to_cloud_tile.dart
+++ lib/views/settings/account/widgets/save_settings_to_cloud_tile.dart
@@ -1,8 +1,8 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:riverpod/src/state_provider.dart';
 
 import '../../../../controllers/settings/firestore_preferences_controller.dart';
 import '../../../../controllers/settings/preferences_controller.dart';
