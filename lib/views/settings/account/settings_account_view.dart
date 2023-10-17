420198a947309e0c4b996d98671ae35f71816f8e
mikezamayias <29067825+mikezamayias@users.noreply.github.com>
2023-10-17 13:53:27 +0300

    Hides prompt to save user preferences in Firestore if already present.

--- lib/views/settings/account/settings_account_view.dart
+++ lib/views/settings/account/settings_account_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart' hide AppBar, ListTile, PageController;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../controllers/settings/firestore_preferences_controller.dart';
 import '../../../extensions/widget_extensions.dart';
+import '../../../models/settings/preference_model.dart';
 import '../../../utils/constants.dart';
 import '../../../widgets/app_bar.dart';
 import '../../blueprint/blueprint_view.dart';
@@ -14,26 +16,37 @@ class SettingsAccountView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    List<Widget> pageWidgets = [
-      const EditNameTile(),
-      const SaveSettingsToCloudTile(),
 
-      /// TODO: study and implement the re-authentication process required
-      ///  to change email
-      // const EditEmailTile(),
-      const SignOutTile(),
-      // const DeleteAccountTile(),
-    ].animateWidgetList();
 
     return BlueprintView(
       appBar: const AppBar(
         automaticallyImplyLeading: true,
         pathNames: ['Settings', 'Account'],
       ),
-      body: Wrap(
-        spacing: gap,
-        runSpacing: gap,
-        children: pageWidgets,
+      body: StreamBuilder<List<PreferenceModel>>(
+        stream: FirestorePreferencesController().getPreferences(),
+        builder: (
+          BuildContext context,
+          AsyncSnapshot<List<PreferenceModel>> snapshot,
+        ) {
+          List<Widget> pageWidgets = [
+            const EditNameTile(),
+
+            if (snapshot.hasData && snapshot.data!.isEmpty)
+              const SaveSettingsToCloudTile(),
+
+            /// TODO: study and implement the re-authentication process required
+            ///  to change email
+            // const EditEmailTile(),
+            const SignOutTile(),
+            // const DeleteAccountTile(),
+          ].animateWidgetList();
+          return Wrap(
+            spacing: gap,
+            runSpacing: gap,
+            children: pageWidgets,
+          );
+        },
       ),
     );
   }
