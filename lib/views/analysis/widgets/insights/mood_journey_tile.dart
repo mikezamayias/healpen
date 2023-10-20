be5f19bdb662b358fcf298c35bee972b759c02d8
Mike Zamayias <29067825+mikezamayias@users.noreply.github.com>
2023-10-21 02:17:06 +0300

    the blue is the spline graph and the green is the text divider

--- lib/views/analysis/widgets/insights/mood_journey_tile.dart
+++ lib/views/analysis/widgets/insights/mood_journey_tile.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../../../../widgets/to_be_implemented_tile.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 class MoodJourneyTile extends ConsumerWidget {
   const MoodJourneyTile({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const ToBeImplementedTile();
+    return ListView.separated(
+      reverse: true,
+      shrinkWrap: true,
+      itemBuilder: (_, __) => SizedBox(
+        height: 10.h,
+        child: Container(
+          color: Colors.blue,
+        ),
+      ),
+      separatorBuilder: (_, __) => SizedBox(
+        height: 5.h,
+        child: Container(
+          color: Colors.green,
+        ),
+      ),
+      itemCount: 3,
+    );
   }
 }
