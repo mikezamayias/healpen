0fb292705fb08160e19b26bada0814b7df812435
Mike Zamayias <29067825+mikezamayias@users.noreply.github.com>
2023-10-20 14:48:32 +0300

    adds new navigation setting

---
+++ lib/views/settings/navigation/widgets/smaller_navigation_elements_tile.dart
@@ -0,0 +1,50 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../controllers/settings/firestore_preferences_controller.dart';
+import '../../../../controllers/settings/preferences_controller.dart';
+import '../../../../providers/settings_providers.dart';
+import '../../../../utils/constants.dart';
+import '../../../../utils/helper_functions.dart';
+import '../../../../widgets/custom_list_tile.dart';
+
+class SmallerNavigationElementsTile extends ConsumerWidget {
+  const SmallerNavigationElementsTile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return CustomListTile(
+      contentPadding: EdgeInsets.all(gap),
+      titleString: 'Enable back button in app bar',
+      explanationString:
+          'Shows a back button at the top of the screen, making it simpler to '
+          'return to previous pages.',
+      enableExplanationWrapper: true,
+      trailing: Switch(
+        value: ref.watch(navigationSmallerNavigationElementsProvider),
+        onChanged: (value) {
+          vibrate(
+            ref.watch(navigationEnableHapticFeedbackProvider),
+            () async {
+              ref
+                  .read(navigationSmallerNavigationElementsProvider.notifier)
+                  .state = value;
+              await FirestorePreferencesController.instance.savePreference(
+                PreferencesController.navigationSmallerNavigationElements
+                    .withValue(
+                  ref.watch(navigationSmallerNavigationElementsProvider),
+                ),
+              );
+              log(
+                '${ref.watch(navigationSmallerNavigationElementsProvider)}',
+                name: 'SmallerNavigationElementsTile',
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
