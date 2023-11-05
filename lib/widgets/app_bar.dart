bc91393a34de345d5508f725f402c1383230581e
Mike Zamayias <29067825+mikezamayias@users.noreply.github.com>
2023-11-05 02:35:48 +0200

    makes app bar work in license views

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -26,29 +26,28 @@ class AppBar extends ConsumerWidget {
         ref.watch(navigationSmallerNavigationElementsProvider);
     final appBarContent = RichText(
       text: TextSpan(
+        style: const TextStyle(),
         children: [
           for (int i = 0; i < pathNames.length; i++)
-            TextSpan(
-              text: i < pathNames.length - 1
-                  ? '${pathNames[i]} / '
-                  : pathNames.length != 1 && pathNames.length > 2
-                      ? '\n${pathNames[i]}'
-                      : pathNames[i],
-              style: (i < pathNames.length - 1)
-                  ? context.theme.textTheme.titleLarge!.copyWith(
-                      color: switch (smallNavigationElements) {
-                        true => context.theme.colorScheme.outline,
-                        false => context.theme.colorScheme.onSurfaceVariant,
-                      },
-                      overflow: TextOverflow.ellipsis,
-                    )
-                  : context.theme.textTheme.headlineSmall!.copyWith(
-                      color: switch (smallNavigationElements) {
-                        true => context.theme.colorScheme.secondary,
-                        false => context.theme.colorScheme.onSurfaceVariant,
-                      },
-                      overflow: TextOverflow.ellipsis,
-                    ),
+            WidgetSpan(
+              child: Text(
+                i < pathNames.length - 1
+                    ? '${pathNames[i]} / '
+                    : pathNames[i],
+                style: (i < pathNames.length - 1)
+                    ? context.theme.textTheme.titleLarge!.copyWith(
+                        color: smallNavigationElements
+                            ? context.theme.colorScheme.outline
+                            : context.theme.colorScheme.onSurfaceVariant,
+                      )
+                    : context.theme.textTheme.headlineSmall!.copyWith(
+                        color: smallNavigationElements
+                            ? context.theme.colorScheme.secondary
+                            : context.theme.colorScheme.onSurfaceVariant,
+                      ),
+                softWrap: true,
+                overflow: TextOverflow.ellipsis,
+              ),
             ),
         ],
       ),
@@ -78,7 +77,7 @@ class AppBar extends ConsumerWidget {
                 icon: const FaIcon(FontAwesomeIcons.chevronLeft),
               ),
               SizedBox(width: gap),
-              appBarContent,
+              Flexible(child: appBarContent),
             ],
           )
         : appBarContent;
