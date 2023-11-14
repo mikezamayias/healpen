f3252d52eb3874e4569955901f4e7c7f3a9a1d82
Mike Zamayias <29067825+mikezamayias@users.noreply.github.com>
2023-11-14 15:08:54 +0200

    tweaks padding

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart' hide AppBar, Page;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screwdriver/flutter_screwdriver.dart';
@@ -5,7 +7,6 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 import '../providers/settings_providers.dart';
-import '../route_controller.dart';
 import '../utils/constants.dart';
 import '../utils/helper_functions.dart';
 
@@ -26,30 +27,40 @@ class AppBar extends ConsumerWidget {
     final showBackButton = ref.watch(navigationShowBackButtonProvider);
     final smallNavigationElements =
         ref.watch(navigationSmallerNavigationElementsProvider);
-    final appBarContent = RichText(
-      text: TextSpan(
-        style: const TextStyle(),
-        children: [
-          for (int i = 0; i < pathNames.length; i++)
-            WidgetSpan(
-              child: Text(
-                i < pathNames.length - 1 ? '${pathNames[i]} / ' : pathNames[i],
-                style: (i < pathNames.length - 1)
-                    ? context.theme.textTheme.titleLarge!.copyWith(
-                        color: smallNavigationElements
-                            ? context.theme.colorScheme.outline
-                            : context.theme.colorScheme.onSurfaceVariant,
-                      )
-                    : context.theme.textTheme.headlineSmall!.copyWith(
-                        color: smallNavigationElements
-                            ? context.theme.colorScheme.secondary
-                            : context.theme.colorScheme.onSurfaceVariant,
-                      ),
-                softWrap: true,
-                overflow: TextOverflow.ellipsis,
+    final appBarContent = AnimatedContainer(
+      duration: standardDuration,
+      curve: standardCurve,
+      padding:
+          showBackButton && pathNames.length < 3 && automaticallyImplyLeading!
+              ? EdgeInsets.only(bottom: gap)
+              : EdgeInsets.zero,
+      child: RichText(
+        text: TextSpan(
+          style: const TextStyle(),
+          children: [
+            for (int i = 0; i < pathNames.length; i++)
+              WidgetSpan(
+                child: Text(
+                  i < pathNames.length - 1
+                      ? '${pathNames[i]} / '
+                      : pathNames[i],
+                  style: (i < pathNames.length - 1)
+                      ? context.theme.textTheme.titleLarge!.copyWith(
+                          color: smallNavigationElements
+                              ? context.theme.colorScheme.outline
+                              : context.theme.colorScheme.onSurfaceVariant,
+                        )
+                      : context.theme.textTheme.headlineSmall!.copyWith(
+                          color: smallNavigationElements
+                              ? context.theme.colorScheme.secondary
+                              : context.theme.colorScheme.onSurfaceVariant,
+                        ),
+                  softWrap: true,
+                  overflow: TextOverflow.ellipsis,
+                ),
               ),
-            ),
-        ],
+          ],
+        ),
       ),
     );
 
@@ -84,13 +95,14 @@ class AppBar extends ConsumerWidget {
             ],
           )
         : appBarContent;
+    log('${context.theme.colorScheme.primary}', name: 'primary');
+    log('${context.theme.colorScheme.surface}', name: 'surface');
+    log('${context.theme.colorScheme.background}', name: 'background');
+    log('${context.theme.colorScheme.surfaceVariant}', name: 'surfaceVariant');
     return AnimatedContainer(
       duration: standardDuration,
       curve: standardCurve,
-      padding: ModalRoute.of(context)?.settings.name !=
-              RouterController.authWrapperRoute.route
-          ? EdgeInsets.zero
-          : EdgeInsets.only(bottom: gap),
+      padding: EdgeInsets.only(bottom: gap),
       child: AnimatedContainer(
         duration: standardDuration,
         curve: standardCurve,
