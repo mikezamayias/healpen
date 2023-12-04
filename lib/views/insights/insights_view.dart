691bb4133bdde61bbfc40a784033c9b5cc1e7c5d
Mike Zamayias <>
2023-12-04 13:45:47 +0200

    Fixes analysis data not showing correctly

--- lib/views/insights/insights_view.dart
+++ lib/views/insights/insights_view.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart' hide AppBar, PageController;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../controllers/analysis_view_controller.dart';
 import '../../controllers/page_controller.dart';
 import '../../providers/settings_providers.dart';
 import '../../widgets/app_bar.dart';
@@ -23,7 +24,7 @@ class InsightsView extends ConsumerWidget {
               .titleGenerator(FirebaseAuth.instance.currentUser?.displayName)
         ],
       ),
-      body: (ref.watch(showAnalyzeButtonProvider))
+      body: (ref.watch(analysisModelListProvider).isEmpty)
           ? const NoAnalysisFoundTile()
           : const InsightsTile(),
     );
