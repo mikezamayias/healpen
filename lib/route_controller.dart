9244316b882765479e4116c78d1aa22d61e9cb38
Mike Zamayias <>
2023-12-14 16:54:25 +0200

    Add route and navigation functionality

--- lib/route_controller.dart
+++ lib/route_controller.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart' hide PageController;
 
 import 'controllers/page_controller.dart';
 import 'models/page_model.dart';
+import 'utils/constants.dart';
 
 class RouterController {
 //   make it singleton
@@ -40,10 +41,56 @@ class RouterController {
     healpen,
   ];
 
-//   create a method that will return a map of string and widget based on the
-//   above routes
+  //   create a method that will return a map of string and widget based on the
+  //   above routes
   Map<String, Widget Function(BuildContext)> get routes => {
         for (var route in routeList)
           route.route: (context) => route.pageModel.widget,
       };
+
+  static ({PageModel pageModel, String route}) getRoute(String routeName) {
+    return RouterController().routeList.firstWhere(
+          (element) => element.route == routeName,
+          orElse: () => RouterController().routeList.first,
+        );
+  }
+
+  Route? onGenerateRoute(RouteSettings settings) {
+    final routeName = settings.name!;
+    final builder = PageRouteBuilder(
+      settings: settings,
+      transitionDuration: emphasizedDuration,
+      reverseTransitionDuration: emphasizedDuration,
+      pageBuilder: (
+        context,
+        animation,
+        secondaryAnimation,
+      ) {
+        return RouterController.getRoute(routeName).pageModel.widget;
+      },
+      transitionsBuilder: (
+        context,
+        animation,
+        secondaryAnimation,
+        child,
+      ) {
+        final curvedAnimation = CurvedAnimation(
+          parent: animation,
+          curve: emphasizedCurve,
+          reverseCurve: emphasizedCurve.flipped,
+        );
+        return SlideTransition(
+          position: Tween<Offset>(
+            begin: const Offset(1, 0),
+            end: Offset.zero,
+          ).animate(curvedAnimation),
+          child: FadeTransition(
+            opacity: animation,
+            child: child,
+          ),
+        );
+      },
+    );
+    return builder;
+  }
 }
