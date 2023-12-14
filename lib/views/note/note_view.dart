aae30918281bd21d80e06dcb433c91252f0573e8
Mike Zamayias <>
2023-12-14 17:12:34 +0200

    Reimplement widget

--- lib/views/note/note_view.dart
+++ lib/views/note/note_view.dart
@@ -5,7 +5,6 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
-import '../../controllers/emotional_echo_controller.dart';
 import '../../extensions/int_extensions.dart';
 import '../../models/analysis/analysis_model.dart';
 import '../../models/sentence/sentence_model.dart';
@@ -17,18 +16,23 @@ import '../../widgets/app_bar.dart';
 import '../../widgets/custom_list_tile.dart';
 import '../blueprint/blueprint_view.dart';
 import '../insights/widgets/insights/emotional_echo/emotional_echo_tile.dart';
+import '../simple/simple_blueprint_view.dart';
+import '../simple/widgets/simple_app_bar.dart';
 
-class NoteView extends ConsumerWidget {
+class NoteView extends ConsumerStatefulWidget {
   const NoteView({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<NoteView> createState() => _NoteViewState();
+}
+
+class _NoteViewState extends ConsumerState<NoteView> {
+  @override
+  Widget build(BuildContext context) {
     final args = ModalRoute.of(context)!.settings.arguments as ({
       AnalysisModel analysisModel
     });
     final AnalysisModel analysisModel = args.analysisModel;
-    ref.watch(emotionalEchoControllerProvider).sentimentScore =
-        analysisModel.score;
     logger.i(
       analysisModel.sentences.toString(),
     );
@@ -37,125 +41,104 @@ class NoteView extends ConsumerWidget {
         sentence.toString(),
       );
     }
-    return BlueprintView(
-      appBar: AppBar(
-        pathNames: [
-          DateFormat('EEE d MMM yyyy').format(
-            DateTime.fromMillisecondsSinceEpoch(analysisModel.timestamp),
-          ),
-        ],
-        automaticallyImplyLeading: true,
-      ),
-      body: ClipRRect(
-        borderRadius: BorderRadius.circular(radius),
-        child: SingleChildScrollView(
-          child: Wrap(
-            spacing: gap,
-            runSpacing: gap,
-            children: [
-              CustomListTile(
-                responsiveWidth: true,
-                useSmallerNavigationSetting:
-                    !ref.watch(navigationSmallerNavigationElementsProvider),
-                enableExplanationWrapper:
-                    !ref.watch(navigationSmallerNavigationElementsProvider),
-                titleString: 'Time',
-                subtitleString: DateFormat('HH:mm:ss').format(
-                  DateTime.fromMillisecondsSinceEpoch(analysisModel.timestamp),
-                ),
-                subtitlePadding:
-                    ref.watch(navigationSmallerNavigationElementsProvider)
-                        ? EdgeInsets.zero
-                        : EdgeInsets.all(gap),
+    Widget body = ClipRRect(
+      borderRadius: BorderRadius.circular(gap),
+      child: SingleChildScrollView(
+        child: Wrap(
+          spacing: gap,
+          runSpacing: gap,
+          children: [
+            buildCustomListTile(
+              'Time',
+              DateFormat('HH:mm:ss').format(
+                DateTime.fromMillisecondsSinceEpoch(analysisModel.timestamp),
               ),
-              CustomListTile(
-                useSmallerNavigationSetting:
-                    !ref.watch(navigationSmallerNavigationElementsProvider),
-                enableExplanationWrapper:
-                    !ref.watch(navigationSmallerNavigationElementsProvider),
-                responsiveWidth: true,
-                titleString: 'Duration',
-                subtitleString: analysisModel.duration.writingDurationFormat(),
-                subtitlePadding:
-                    ref.watch(navigationSmallerNavigationElementsProvider)
-                        ? EdgeInsets.zero
-                        : EdgeInsets.all(gap),
+            ),
+            buildCustomListTile(
+              'Duration',
+              analysisModel.duration.writingDurationFormat(),
+            ),
+            CustomListTile(
+              responsiveWidth: false,
+              useSmallerNavigationSetting:
+                  !ref.watch(navigationSmallerNavigationElementsProvider),
+              enableExplanationWrapper:
+                  !ref.watch(navigationSmallerNavigationElementsProvider),
+              titleString: 'Content',
+              subtitleString: analysisModel.content,
+              selectableText: true,
+              subtitlePadding:
+                  ref.watch(navigationSmallerNavigationElementsProvider)
+                      ? EdgeInsets.zero
+                      : EdgeInsets.all(gap),
+            ),
+            CustomListTile(
+              useSmallerNavigationSetting:
+                  !ref.watch(navigationSmallerNavigationElementsProvider),
+              enableExplanationWrapper:
+                  !ref.watch(navigationSmallerNavigationElementsProvider),
+              titleString: 'Emotional Echo',
+              subtitle: SizedBox(
+                height: 39.h,
+                child: const EmotionalEchoTile(),
               ),
-              CustomListTile(
-                responsiveWidth: true,
-                useSmallerNavigationSetting:
-                    !ref.watch(navigationSmallerNavigationElementsProvider),
-                enableExplanationWrapper:
-                    !ref.watch(navigationSmallerNavigationElementsProvider),
-                titleString: 'Word Count',
-                subtitleString: '${analysisModel.wordCount}',
-                subtitlePadding:
-                    ref.watch(navigationSmallerNavigationElementsProvider)
-                        ? EdgeInsets.zero
-                        : EdgeInsets.all(gap),
-              ),
-              CustomListTile(
-                responsiveWidth: true,
-                useSmallerNavigationSetting:
-                    !ref.watch(navigationSmallerNavigationElementsProvider),
-                enableExplanationWrapper:
-                    !ref.watch(navigationSmallerNavigationElementsProvider),
-                titleString: 'Content',
-                subtitleString: analysisModel.content,
-                selectableText: true,
-                subtitlePadding:
-                    ref.watch(navigationSmallerNavigationElementsProvider)
-                        ? EdgeInsets.zero
-                        : EdgeInsets.all(gap),
-              ),
-              CustomListTile(
-                useSmallerNavigationSetting:
-                    !ref.watch(navigationSmallerNavigationElementsProvider),
-                enableExplanationWrapper:
-                    !ref.watch(navigationSmallerNavigationElementsProvider),
-                titleString: 'Emotional Echo',
-                subtitle: SizedBox(
-                  height: 39.h,
-                  child: const EmotionalEchoTile(),
-                ),
-              ),
-              CustomListTile(
-                responsiveWidth: true,
-                useSmallerNavigationSetting:
-                    !ref.watch(navigationSmallerNavigationElementsProvider),
-                enableExplanationWrapper:
-                    !ref.watch(navigationSmallerNavigationElementsProvider),
-                titleString: 'Icon',
-                subtitle: FaIcon(
-                  getSentimentIcon(analysisModel.score),
-                  color: getShapeColorOnSentiment(
-                    context.theme,
-                    analysisModel.score,
-                  ),
-                  size: 10.h,
-                ),
-                subtitlePadding:
-                    ref.watch(navigationSmallerNavigationElementsProvider)
-                        ? EdgeInsets.zero
-                        : EdgeInsets.all(gap),
-              ),
-              CustomListTile(
-                responsiveWidth: true,
-                useSmallerNavigationSetting:
-                    !ref.watch(navigationSmallerNavigationElementsProvider),
-                enableExplanationWrapper:
-                    !ref.watch(navigationSmallerNavigationElementsProvider),
-                titleString: 'Sentence Count',
-                subtitleString: '${analysisModel.sentences.length}',
-                subtitlePadding:
-                    ref.watch(navigationSmallerNavigationElementsProvider)
-                        ? EdgeInsets.zero
-                        : EdgeInsets.all(gap),
-              ),
-            ],
-          ),
+            ),
+            buildCustomListTile(
+              'Sentence Count',
+              '${analysisModel.sentences.length}',
+            ),
+            buildCustomListTile(
+              'Word Count',
+              '${analysisModel.wordCount}',
+            ),
+          ],
         ),
       ),
+    );
+    final titleString = DateFormat('EEE d MMM yyyy').format(
+      DateTime.fromMillisecondsSinceEpoch(analysisModel.timestamp),
+    );
+    return ref.watch(navigationSimpleUIProvider)
+        ? SimpleBlueprintView(
+            simpleUiAppBar: SimpleAppBar(
+              appBarTitleString: titleString,
+              appBarTrailing: FaIcon(
+                getSentimentIcon(analysisModel.score),
+                color: getShapeColorOnSentiment(
+                  context.theme,
+                  analysisModel.score,
+                ),
+                size: context.theme.textTheme.displaySmall!.fontSize,
+              ),
+            ),
+            body: body,
+          )
+        : BlueprintView(
+            appBar: AppBar(
+              pathNames: [titleString],
+              automaticallyImplyLeading: true,
+            ),
+            body: body,
+          );
+  }
+
+  Widget buildCustomListTile(String fieldName, dynamic fieldValue) {
+    assert(fieldValue is String || fieldValue is Widget);
+    return CustomListTile(
+      useSmallerNavigationSetting:
+          !ref.watch(navigationSmallerNavigationElementsProvider),
+      enableExplanationWrapper:
+          !ref.watch(navigationSmallerNavigationElementsProvider),
+      titleString: fieldName,
+      trailing: fieldValue is Widget
+          ? fieldValue
+          : Text(
+              fieldValue,
+              style: context.theme.textTheme.titleLarge,
+            ),
+      subtitlePadding: ref.watch(navigationSmallerNavigationElementsProvider)
+          ? EdgeInsets.zero
+          : EdgeInsets.all(gap),
     );
   }
 }
