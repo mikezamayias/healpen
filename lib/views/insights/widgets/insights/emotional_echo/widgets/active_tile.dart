f27109a9a09b7f759cd4b4cd2de7132985fd0308
Mike Zamayias <>
2023-12-14 17:12:08 +0200

    Refactor EmotionalEchoActiveTile widget implementation

--- lib/views/insights/widgets/insights/emotional_echo/widgets/active_tile.dart
+++ lib/views/insights/widgets/insights/emotional_echo/widgets/active_tile.dart
@@ -11,122 +11,137 @@ import '../../../../../../utils/helper_functions.dart';
 import '../../../../../../utils/logger.dart';
 import 'inactive_tile.dart';
 
-class EmotionalEchoActiveTile extends ConsumerWidget {
+class EmotionalEchoActiveTile extends ConsumerStatefulWidget {
   const EmotionalEchoActiveTile({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    double sentiment = ref
-        .watch(emotionalEchoControllerProvider)
-        .sentimentScore
-        .withDecimalPlaces(1);
+  ConsumerState<EmotionalEchoActiveTile> createState() =>
+      _EmotionalEchoActiveTileState();
+}
+
+class _EmotionalEchoActiveTileState
+    extends ConsumerState<EmotionalEchoActiveTile> {
+  TextStyle get titleLargeStyle =>
+      textTheme.titleLarge!.copyWith(fontWeight: FontWeight.w600);
+  TextStyle get titleSmallStyle =>
+      textTheme.titleSmall!.copyWith(fontWeight: FontWeight.w600);
+  Color get background => theme.colorScheme.background;
+  double get sentiment =>
+      ref.watch(EmotionalEchoController.scoreProvider).withDecimalPlaces(2);
+  bool get isActive => !ref.watch(EmotionalEchoController.isPressedProvider);
+  bool get enableInformatoryText => ref.watch(navigationShowInfoProvider);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        _buildEmotionalEchoInactiveTile(),
+        _buildAnimatedSentimentContainer(),
+      ],
+    );
+  }
+
+  Widget _buildEmotionalEchoInactiveTile() {
+    return AnimatedPositioned(
+      duration: standardDuration,
+      curve: standardCurve,
+      top: 0,
+      bottom: 0,
+      right: isActive ? -99.w : 0,
+      left: 0,
+      child: Padding(
+        padding: EdgeInsets.all(radius),
+        child: const EmotionalEchoInactiveTile(),
+      ),
+    );
+  }
+
+  Widget _buildAnimatedSentimentContainer() {
     logger.i(
       '[EmotionalEchoActiveTile:sentiment] ${sentiment.toStringAsFixed(1)}',
     );
-    final sentimentLadder = sentimentValueToLabel
-      ..addAll({sentiment.withDecimalPlaces(1): 'Current Mood'});
-    // Sort the map by key.
-    final sortedSentimentLadder = Map.fromEntries(
-      sentimentLadder.entries.toList()
-        ..sort((MapEntry<num, String> a, MapEntry<num, String> b) =>
-            b.key.compareTo(a.key)),
-    );
-    final isActive = ref.watch(EmotionalEchoController.isPressedProvider);
-    final enableInformatoryText = ref.watch(navigationShowInfoProvider);
-    return Stack(
-      children: <Widget>[
-        AnimatedPositioned(
-          duration: standardDuration,
-          curve: standardCurve,
-          top: 0,
-          bottom: 0,
-          right: isActive ? -99.w : 0,
-          left: 0,
-          child: const EmotionalEchoInactiveTile(),
-        ),
-        AnimatedPositioned(
-          duration: standardDuration,
-          curve: standardCurve,
-          top: 0,
-          bottom: 0,
-          left: isActive ? 0 : -71.w,
-          right: 0,
-          child: AnimatedContainer(
-            duration: standardDuration,
-            curve: standardCurve,
-            decoration: isActive
-                ? BoxDecoration(
-                    gradient: isActive
-                        ? LinearGradient(
-                            begin: Alignment.centerLeft,
-                            end: Alignment.centerRight,
-                            colors: [
-                              context.theme.colorScheme.background
-                                  .withOpacity(1.0),
-                              context.theme.colorScheme.background
-                                  .withOpacity(0.0),
-                            ],
-                            stops: const [0.0, 1.0],
-                          )
-                        : null,
-                  )
-                : const BoxDecoration(),
-            width: 51.w,
-            child: Stack(
-              children: [
-                Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: sortedSentimentLadder.keys.map(
-                    (num key) {
-                      return Visibility(
-                        visible: key == sentiment,
-                        child: Text(
-                          enableInformatoryText
-                              ? '${key.toStringAsFixed(1)}, ${sortedSentimentLadder[key]}'
-                              : '${sortedSentimentLadder[key]}',
-                          style: context.theme.textTheme.titleLarge!.copyWith(
-                            fontWeight: FontWeight.w600,
-                            color: getShapeColorOnSentiment(
-                              context.theme,
-                              key,
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ).toList(),
+    return AnimatedPositioned(
+      duration: standardDuration,
+      curve: standardCurve,
+      top: 0,
+      bottom: 0,
+      left: isActive ? 0 : -71.w,
+      right: 0,
+      child: AnimatedContainer(
+        duration: standardDuration,
+        curve: standardCurve,
+        decoration: isActive
+            ? BoxDecoration(
+                borderRadius: BorderRadius.circular(radius),
+                gradient: LinearGradient(
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                  colors: [
+                    background.withOpacity(1.0),
+                    background.withOpacity(0.0),
+                  ],
+                  stops: const [0.0, 1.0],
                 ),
-                Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: sentimentLabels.reversed.map(
-                    (String label) {
-                      int labelIndex = sentimentLabels.indexOf(label);
-                      return Visibility(
-                        visible: !scoreIsCloseToValue(
-                            sentiment, sentimentValues.elementAt(labelIndex)),
-                        child: Text(
-                          enableInformatoryText
-                              ? '${sentimentValues.elementAt(labelIndex)}, $label'
-                              : label,
-                          style: context.theme.textTheme.titleSmall!.copyWith(
-                            fontWeight: FontWeight.w600,
-                            color:
-                                getShapeColorOnSentiment(context.theme, label),
-                          ),
-                        ),
-                      );
-                    },
-                  ).toList(),
-                ),
-              ],
-            ),
+              )
+            : BoxDecoration(
+                borderRadius: BorderRadius.circular(radius),
+              ),
+        width: 51.w,
+        child: Padding(
+          padding: EdgeInsets.all(gap),
+          child: Stack(
+            children: [
+              currentText(),
+              Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: sentimentLabels.reversed.map(scaleText).toList(),
+              ),
+            ],
           ),
         ),
-      ],
+      ),
+    );
+  }
+
+  Positioned currentText() {
+    // Get the available height using MediaQuery
+    final double availableHeight = 39.h;
+
+    // Example range for sentiment scores
+    const double minSentiment = 0.0;
+    const double maxSentiment = 1.0;
+
+    // Normalize sentiment score to available height
+    final double normalizedSentiment =
+        (sentiment - minSentiment) / (maxSentiment - minSentiment);
+    final double topPosition =
+        normalizedSentiment * availableHeight + sentiment < 0
+            ? titleLargeStyle.fontSize! * 2
+            : -titleLargeStyle.fontSize! / 2;
+
+    // Rest of the method
+    const sentimentText = 'Current Mood';
+    final sentimentColor = getShapeColorOnSentiment(theme, sentiment);
+
+    return Positioned(
+      left: 0,
+      bottom: topPosition, // Dynamically calculated position
+      child: Text(
+        enableInformatoryText ? '$sentiment, $sentimentText' : sentimentText,
+        style: titleLargeStyle.copyWith(color: sentimentColor),
+      ),
+    );
+  }
+
+  Text scaleText(String label) {
+    int labelIndex = sentimentLabels.indexOf(label);
+    final sentimentValue = sentimentValues.elementAt(labelIndex);
+    final sentimentColor = getShapeColorOnSentiment(theme, label);
+    return Text(
+      enableInformatoryText ? '$sentimentValue, $label' : label,
+      style: titleSmallStyle.copyWith(color: sentimentColor),
     );
   }
 }
