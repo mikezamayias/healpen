613b5619f1b040b3d19164677cffe110095db5dc
Mike Zamayias <>
2023-12-20 17:26:02 +0200

    Refactors SimpleBlueprintView

--- lib/views/simple/simple_blueprint_view.dart
+++ lib/views/simple/simple_blueprint_view.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screwdriver/flutter_screwdriver.dart';
 
+import '../../providers/settings_providers.dart';
 import '../../utils/constants.dart';
-import '../blueprint/blueprint_view.dart';
+import '../../utils/helper_functions.dart';
 import 'widgets/simple_app_bar.dart';
 import 'widgets/simple_body.dart';
 
 class SimpleBlueprintView extends ConsumerWidget {
-  final SimpleAppBar simpleUiAppBar;
+  final SimpleAppBar simpleAppBar;
   final EdgeInsets? bodyPadding;
   final Widget body;
 
   const SimpleBlueprintView({
     super.key,
-    required this.simpleUiAppBar,
+    required this.simpleAppBar,
     this.bodyPadding,
     required this.body,
   });
@@ -22,20 +24,28 @@ class SimpleBlueprintView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final padding = bodyPadding ?? EdgeInsets.all(radius);
-    return BlueprintView(
-      padBodyHorizontally: false,
-      body: Container(
-        color: context.theme.colorScheme.primaryContainer,
-        child: Column(
-          children: <Widget>[
-            simpleUiAppBar,
-            Expanded(
-              child: SimpleBody(
-                padding: padding,
-                body: body,
-              ),
+    return Material(
+      child: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: getSystemUIOverlayStyle(
+          context.theme,
+          ref.watch(themeAppearanceProvider),
+        ),
+        child: GestureDetector(
+          onTap: () => context.focusScope.unfocus(),
+          child: Container(
+            color: context.theme.colorScheme.primaryContainer,
+            child: Column(
+              children: <Widget>[
+                simpleAppBar,
+                Expanded(
+                  child: SimpleBody(
+                    padding: padding,
+                    body: body,
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
