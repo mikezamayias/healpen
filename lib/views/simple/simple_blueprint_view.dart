ab3b1808c0debd26087012de1e7d2508ec5e4c03
Mike Zamayias <>
2023-12-20 17:22:11 +0200

    Refactor body on SimpleBlueprintView

--- lib/views/simple/simple_blueprint_view.dart
+++ lib/views/simple/simple_blueprint_view.dart
@@ -5,6 +5,7 @@ import 'package:flutter_screwdriver/flutter_screwdriver.dart';
 import '../../utils/constants.dart';
 import '../blueprint/blueprint_view.dart';
 import 'widgets/simple_app_bar.dart';
+import 'widgets/simple_body.dart';
 
 class SimpleBlueprintView extends ConsumerWidget {
   final SimpleAppBar simpleUiAppBar;
@@ -29,26 +30,9 @@ class SimpleBlueprintView extends ConsumerWidget {
           children: <Widget>[
             simpleUiAppBar,
             Expanded(
-              child: AnimatedContainer(
-                duration: standardDuration,
-                curve: standardCurve,
-                alignment: Alignment.topCenter,
-                padding: EdgeInsets.only(
-                  left: radius * 2 - padding.left,
-                  right: radius * 2 - padding.right,
-                  top: radius * 2 - padding.top, 
-                ),
-                decoration: BoxDecoration(
-                  color: context.theme.colorScheme.surface,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(radius * 2),
-                    topRight: Radius.circular(radius * 2),
-                  ),
-                ),
-                child: SafeArea(
-                  top: false,
-                  child: body,
-                ),
+              child: SimpleBody(
+                padding: padding,
+                body: body,
               ),
             ),
           ],
---
+++ lib/views/simple/widgets/simple_body.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screwdriver/flutter_screwdriver.dart';
+
+import '../../../utils/constants.dart';
+
+class SimpleBody extends StatelessWidget {
+  const SimpleBody({
+    super.key,
+    required this.padding,
+    required this.body,
+  });
+
+  final EdgeInsets padding;
+  final Widget body;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: standardDuration,
+      curve: standardCurve,
+      alignment: Alignment.topCenter,
+      padding: EdgeInsets.only(
+        left: radius * 2 - padding.left,
+        right: radius * 2 - padding.right,
+        top: radius * 2 - padding.top,
+      ),
+      decoration: BoxDecoration(
+        color: context.theme.colorScheme.surface,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(radius * 2),
+          topRight: Radius.circular(radius * 2),
+        ),
+      ),
+      child: SafeArea(
+        top: false,
+        child: body,
+      ),
+    );
+  }
+}
