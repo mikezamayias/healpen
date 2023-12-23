81cf0a4ed8875a8c8deda904e22d256b09bfdf6b
Mike Zamayias <>
2023-12-23 18:15:47 +0200

    Refactor note_view.dart and add sentiment_trailing widget

--- lib/views/note/note_view.dart
+++ lib/views/note/note_view.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart' hide AppBar;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screwdriver/flutter_screwdriver.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
-import 'package:simple_gesture_detector/simple_gesture_detector.dart';
-import 'package:sprung/sprung.dart';
 
 import '../../extensions/int_extensions.dart';
 import '../../models/analysis/analysis_model.dart';
 import '../../models/sentence/sentence_model.dart';
 import '../../providers/settings_providers.dart';
 import '../../utils/constants.dart';
-import '../../utils/helper_functions.dart';
 import '../../utils/logger.dart';
 import '../../widgets/app_bar.dart';
 import '../../widgets/custom_list_tile.dart';
@@ -21,6 +16,7 @@ import '../blueprint/blueprint_view.dart';
 import '../insights/widgets/insights/emotional_echo/emotional_echo_tile.dart';
 import '../simple/simple_blueprint_view.dart';
 import '../simple/widgets/simple_app_bar.dart';
+import 'widgets/sentiment_trailing.dart';
 
 final showEmojiInTrailingProvider = StateProvider<bool>((ref) => true);
 
@@ -32,6 +28,9 @@ class NoteView extends ConsumerStatefulWidget {
 }
 
 class _NoteViewState extends ConsumerState<NoteView> {
+  bool get useSmallerNavigationElements =>
+      ref.watch(navigationSmallerNavigationElementsProvider);
+  bool get useSimpleUI => ref.watch(navigationSimpleUIProvider);
   @override
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context)!.settings.arguments as ({
@@ -47,7 +46,9 @@ class _NoteViewState extends ConsumerState<NoteView> {
       );
     }
     Widget body = ClipRRect(
-      borderRadius: BorderRadius.circular(gap),
+      borderRadius: BorderRadius.circular(
+        useSimpleUI || useSmallerNavigationElements ? radius : gap,
+      ),
       child: SingleChildScrollView(
         child: Wrap(
           spacing: gap,
@@ -107,59 +108,20 @@ class _NoteViewState extends ConsumerState<NoteView> {
         ? SimpleBlueprintView(
             simpleAppBar: SimpleAppBar(
               appBarTitleString: titleString,
-              appBarTrailing: SimpleGestureDetector(
-                behavior: HitTestBehavior.opaque,
-                onVerticalSwipe: (direction) {
-                  vibrate(
-                    ref.watch(navigationEnableHapticFeedbackProvider),
-                    () {
-                      if (direction == SwipeDirection.up) {
-                        ref.read(showEmojiInTrailingProvider.notifier).state =
-                            false;
-                      } else if (direction == SwipeDirection.down) {
-                        ref.read(showEmojiInTrailingProvider.notifier).state =
-                            true;
-                      }
-                    },
-                  );
-                },
-                child: AnimatedCrossFade(
-                  duration: standardDuration,
-                  reverseDuration: standardDuration,
-                  sizeCurve: Sprung.criticallyDamped,
-                  firstCurve: Sprung.criticallyDamped,
-                  secondCurve: Sprung.criticallyDamped,
-                  crossFadeState: ref.watch(showEmojiInTrailingProvider)
-                      ? CrossFadeState.showFirst
-                      : CrossFadeState.showSecond,
-                  firstChild: FaIcon(
-                    getSentimentIcon(analysisModel.score),
-                    color: getShapeColorOnSentiment(
-                      context.theme,
-                      analysisModel.score,
-                    ),
-                    size: context.theme.textTheme.displaySmall!.fontSize,
-                  ),
-                  secondChild: Text(
-                    analysisModel.score.toStringAsFixed(1),
-                    style: context.theme.textTheme.headlineLarge?.copyWith(
-                      fontFamily: GoogleFonts.mPlusRounded1c().fontFamily,
-                      fontWeight: FontWeight.bold,
-                      color: getShapeColorOnSentiment(
-                        context.theme,
-                        analysisModel.score,
-                      ),
-                    ),
-                  ),
-                ),
+              appBarTrailing: SentimentTrailing(
+                score: analysisModel.score,
               ),
             ),
             body: body,
           )
         : BlueprintView(
+            showAppBar: true,
             appBar: AppBar(
               pathNames: [titleString],
               automaticallyImplyLeading: true,
+              trailingWidget: SentimentTrailing(
+                score: analysisModel.score,
+              ),
             ),
             body: body,
           );
---
+++ lib/views/note/widgets/sentiment_trailing.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screwdriver/flutter_screwdriver.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:simple_gesture_detector/simple_gesture_detector.dart';
+import 'package:sprung/sprung.dart';
+
+import '../../../providers/settings_providers.dart';
+import '../../../utils/constants.dart';
+import '../../../utils/helper_functions.dart';
+import '../note_view.dart';
+
+class SentimentTrailing extends ConsumerWidget {
+  const SentimentTrailing({
+    super.key,
+    required this.score,
+  });
+
+  final double score;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return SimpleGestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onVerticalSwipe: (direction) {
+        vibrate(
+          ref.watch(navigationEnableHapticFeedbackProvider),
+          () {
+            if (direction == SwipeDirection.up) {
+              ref.read(showEmojiInTrailingProvider.notifier).state = false;
+            } else if (direction == SwipeDirection.down) {
+              ref.read(showEmojiInTrailingProvider.notifier).state = true;
+            }
+          },
+        );
+      },
+      child: AnimatedCrossFade(
+        duration: standardDuration,
+        reverseDuration: standardDuration,
+        sizeCurve: Sprung.criticallyDamped,
+        firstCurve: Sprung.criticallyDamped,
+        secondCurve: Sprung.criticallyDamped,
+        crossFadeState: ref.watch(showEmojiInTrailingProvider)
+            ? CrossFadeState.showFirst
+            : CrossFadeState.showSecond,
+        firstChild: FaIcon(
+          getSentimentIcon(score),
+          color: getShapeColorOnSentiment(
+            context.theme,
+            score,
+          ),
+          size: context.theme.textTheme.displaySmall!.fontSize,
+        ),
+        secondChild: Text(
+          score.toStringAsFixed(1),
+          style: context.theme.textTheme.headlineLarge?.copyWith(
+            fontFamily: GoogleFonts.mPlusRounded1c().fontFamily,
+            fontWeight: FontWeight.bold,
+            color: getShapeColorOnSentiment(
+              context.theme,
+              score,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
