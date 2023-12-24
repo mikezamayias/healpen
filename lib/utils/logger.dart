c2ab3f0e0c3d2ad6432d4925dd89c37b36ab959e
Mike Zamayias <>
2023-12-25 01:26:13 +0200

    Update logger definition

--- lib/utils/logger.dart
+++ lib/utils/logger.dart
@@ -4,12 +4,12 @@ final logger = Logger(
   printer: PrefixPrinter(
     PrettyPrinter(
       colors: true,
+      methodCount: 3,
+      errorMethodCount: 6,
       noBoxingByDefault: false,
-      methodCount: 1,
-      errorMethodCount: 5,
-      lineLength: 80,
       printEmojis: true,
       printTime: true,
     ),
+    trace: null,
   ),
 );
