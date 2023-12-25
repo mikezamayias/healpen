c08a27f6740ce8a2c6d594257e6a8d77c72ebe57
Mike Zamayias <>
2023-12-25 22:55:12 +0200

    Refactor about view in settings

--- lib/views/settings/about/settings_about_view.dart
+++ lib/views/settings/about/settings_about_view.dart
@@ -1,40 +1,102 @@
 import 'package:flutter/material.dart' hide AppBar, ListTile, PageController;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../../../utils/constants.dart';
 import '../../../providers/settings_providers.dart';
+import '../../../utils/helper_functions.dart';
 import '../../../widgets/app_bar.dart';
+import '../../../widgets/custom_list_tile.dart';
 import '../../blueprint/blueprint_view.dart';
 import '../../simple/simple_blueprint_view.dart';
 import '../../simple/widgets/simple_app_bar.dart';
-import 'widgets/author_tile.dart';
-import 'widgets/open_source_licenses_tile.dart';
-import 'widgets/privacy_policy_tile.dart';
-import 'widgets/terms_of_service_tile.dart';
+import '../licenses/settings_licenses_view.dart';
 
-class SettingsAboutView extends ConsumerWidget {
+class AboutTileModel {
+  AboutTileModel({
+    required this.title,
+    required this.description,
+    required this.onTap,
+    required this.leadingIconData,
+  });
+
+  final String title;
+  final String description;
+  final void Function()? onTap;
+  final IconData leadingIconData;
+}
+
+class SettingsAboutView extends ConsumerStatefulWidget {
   const SettingsAboutView({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    List<Widget> pageWidgets = [
-      // custom list tile for licenses
-      const AuthorTile(),
-      const TermsOfServiceTile(),
-      const PrivacyPolicyTile(),
-      const OpenSourceLicensesTile(),
+  ConsumerState<SettingsAboutView> createState() => _SettingsAboutViewState();
+}
+
+class _SettingsAboutViewState extends ConsumerState<SettingsAboutView> {
+  @override
+  Widget build(BuildContext context) {
+    final aboutTileModels = <AboutTileModel>[
+      AboutTileModel(
+        title: 'Mike Zamayias',
+        description: 'Visit the developer\'s personal website.',
+        onTap: () {
+          launchUrl(
+            Uri.https('mikezamayias.com'),
+            mode: LaunchMode.externalApplication,
+          );
+        },
+        leadingIconData: FontAwesomeIcons.laptopCode,
+      ),
+      AboutTileModel(
+        title: 'Terms and Conditions',
+        description: 'View the terms and conditions for this app.',
+        onTap: () {
+          launchUrl(
+            Uri.https(
+              'iubenda.com',
+              'terms-and-conditions/29795832',
+            ),
+            mode: LaunchMode.externalApplication,
+          );
+        },
+        leadingIconData: FontAwesomeIcons.fileContract,
+      ),
+      AboutTileModel(
+        title: 'Privacy Policy',
+        description: 'View the privacy policy for this app.',
+        onTap: () {
+          launchUrl(
+            Uri.https(
+              'iubenda.com',
+              'privacy-policy/29795832',
+            ),
+            mode: LaunchMode.externalApplication,
+          );
+        },
+        leadingIconData: FontAwesomeIcons.userShield,
+      ),
+      AboutTileModel(
+        title: 'Open Source Licenses',
+        description: 'View the licenses of the open source packages used.',
+        onTap: () {
+          pushWithAnimation(
+            context: context,
+            widget: const SettingsLicensesView(),
+            dataCallback: null,
+          );
+        },
+        leadingIconData: FontAwesomeIcons.code,
+      ),
     ];
-    Widget body = Wrap(
-      spacing: gap,
-      runSpacing: gap,
-      children: pageWidgets,
-    );
+
     return ref.watch(navigationSimpleUIProvider)
         ? SimpleBlueprintView(
             simpleAppBar: const SimpleAppBar(
               appBarTitleString: 'About',
             ),
-            body: body,
+            body: _buildBody(aboutTileModels),
           )
         : BlueprintView(
             appBar: const AppBar(
@@ -44,7 +106,31 @@ class SettingsAboutView extends ConsumerWidget {
                 'About',
               ],
             ),
-            body: body,
+            body: _buildBody(aboutTileModels),
           );
+  }
+
+  bool get _useSmallerNavigationElements =>
+      ref.watch(navigationSmallerNavigationElementsProvider);
+
+  bool get _showInfo => ref.watch(navigationShowInfoProvider);
+
+  Widget _buildBody(List<AboutTileModel> models) {
+    return Wrap(
+      spacing: gap,
+      runSpacing: gap,
+      children: models.map(_buildTile).toList(),
+    );
+  }
+
+  Widget _buildTile(AboutTileModel tileModel) {
+    return CustomListTile(
+      useSmallerNavigationSetting: !_useSmallerNavigationElements,
+      enableExplanationWrapper: !_useSmallerNavigationElements,
+      titleString: tileModel.title,
+      explanationString: _showInfo ? tileModel.description : null,
+      onTap: tileModel.onTap,
+      leadingIconData: tileModel.leadingIconData,
+    );
   }
 }
