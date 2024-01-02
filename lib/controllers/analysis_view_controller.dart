c65aed6377377ba09a73e5bd65d11e079d1297ac
Mike Zamayias <29067825+mikezamayias@users.noreply.github.com>
2024-01-02 20:09:23 +0200

    Add analysis model extensions and update AnalysisModelList

--- lib/controllers/analysis_view_controller.dart
+++ lib/controllers/analysis_view_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../extensions/analysis_model_extensions.dart';
 import '../models/analysis/analysis_model.dart';
 
 enum AnalysisProgress {
@@ -19,4 +20,8 @@ class AnalysisModelList extends StateNotifier<Set<AnalysisModel>> {
   void addAll(Set<AnalysisModel> models) {
     state = {...state, ...models};
   }
+
+  Set<AnalysisModel> get analysisModels => state;
+
+  double get averageScore => analysisModels.averageScore();
 }
